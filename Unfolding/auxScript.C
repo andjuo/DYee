60ffbc2c28ed13bfebfdf46c7225b92a7fac8d8b
Andrius Juodagalvis <andjuo@hotmail.com>
2013-08-25 00:05:38 +0300

    renamed auxScripts to match other directories

--- Unfolding/auxScript.C
+++ Unfolding/auxScript.C
@@ -1,7 +1,7 @@
 {
-  gROOT->ProcessLine(".L calcDetResponse.C+");
-  //calcDetResponse("../config_files/data_vilnius7TeV_test.conf.py",DYTools::DEBUG_RUN,DYTools::NO_SYST);
-  calcDetResponse("../config_files/data_vilnius7TeV_test.conf.py",DYTools::DEBUG_RUN,DYTools::FSR_STUDY);
-  //plotDYAcceptance("../config_files/data_vilnius7TeV_test.conf.py",DYTools::DEBUG_RUN,DYTools::FSR_STUDY,1.05,1.);
+  gROOT->ProcessLine(".L plotDYAcceptance.C+");
+  //plotDYAcceptance("../config_files/data_vilnius8TeV.conf.py",DYTools::DEBUG_RUN,DYTools::NO_SYST);
+  plotDYAcceptance("../config_files/data_vilnius8TeV.conf.py",DYTools::NORMAL_RUN,DYTools::NO_SYST);
+  //plotDYAcceptance("../config_files/data_vilnius8TeV.conf.py",DYTools::DEBUG_RUN,DYTools::FSR_STUDY,1.05,1.);
 
 }
--- Unfolding/auxScript8TeV.C
+++
@@ -1,7 +0,0 @@
-{
-  gROOT->ProcessLine(".L plotDYAcceptance.C+");
-  //plotDYAcceptance("../config_files/data_vilnius8TeV.conf.py",DYTools::DEBUG_RUN,DYTools::NO_SYST);
-  plotDYAcceptance("../config_files/data_vilnius8TeV.conf.py",DYTools::NORMAL_RUN,DYTools::NO_SYST);
-  //plotDYAcceptance("../config_files/data_vilnius8TeV.conf.py",DYTools::DEBUG_RUN,DYTools::FSR_STUDY,1.05,1.);
-
-}
