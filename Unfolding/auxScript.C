9af219e310a5ac7390b641d107372bc594c6e373
Andrius Juodagalvis <andjuo@hotmail.com>
2013-08-25 20:25:19 +0300

    modified to match the renamed main macro

--- Unfolding/auxScript.C
+++ Unfolding/auxScript.C
@@ -1,7 +1,7 @@
 {
-  gROOT->ProcessLine(".L plotDetResponse.C+");
-  //plotDetResponse("../config_files/data_vilnius8TeV.conf.py",DYTools::DEBUG_RUN,DYTools::NO_SYST);
-  plotDetResponse("../config_files/data_vilnius8TeV.conf.py",DYTools::NORMAL_RUN,DYTools::NO_SYST);
-  //plotDYAcceptance("../config_files/data_vilnius8TeV.conf.py",DYTools::DEBUG_RUN,DYTools::FSR_STUDY,1.05,1.);
+  gROOT->ProcessLine(".L plotUnfoldingMatrix.C+");
+  //plotUnfoldingMatrix("../config_files/data_vilnius8TeV.conf.py",DYTools::DEBUG_RUN,DYTools::NO_SYST);
+  plotUnfoldingMatrix("../config_files/data_vilnius8TeV.conf.py",DYTools::NORMAL_RUN,DYTools::NO_SYST);
+  //plotUnfoldingMatrix("../config_files/data_vilnius8TeV.conf.py",DYTools::DEBUG_RUN,DYTools::FSR_STUDY,1.05,1.);
 
 }
--- Unfolding/auxScript7TeV.C
+++ Unfolding/auxScript7TeV.C
@@ -1,7 +1,10 @@
 {
-  gROOT->ProcessLine(".L plotDetResponse.C+");
-  //calcDetResponse("../config_files/data_vilnius7TeV_test.conf.py",DYTools::DEBUG_RUN,DYTools::NO_SYST);
-  plotDetResponse("../config_files/data_vilnius7TeV_test.conf.py",DYTools::DEBUG_RUN,DYTools::FSR_STUDY);
-  //plotDYAcceptance("../config_files/data_vilnius7TeV_test.conf.py",DYTools::DEBUG_RUN,DYTools::FSR_STUDY,1.05,1.);
+  gROOT->ProcessLine(".L plotUnfoldingMatrix.C+");
+  plotUnfoldingMatrix("../config_files/data_vilnius7TeV_test.conf.py",DYTools::DEBUG_RUN,DYTools::NO_SYST);
+  //plotUnfoldingMatrix("../config_files/data_vilnius7TeV_test.conf.py",DYTools::NORMAL_RUN,DYTools::NO_SYST);
+  //plotUnfoldingMatrix("../config_files/data_vilnius7TeV_test.conf.py",DYTools::LOAD_DATA,DYTools::NO_SYST);
+
+  //plotUnfoldingMatrix("../config_files/data_vilnius7TeV_test.conf.py",DYTools::DEBUG_RUN,DYTools::FSR_STUDY);
+  //plotUnfoldingMatrix("../config_files/data_vilnius7TeV_test.conf.py",DYTools::DEBUG_RUN,DYTools::FSR_STUDY,1.05,1.);
 
 }
