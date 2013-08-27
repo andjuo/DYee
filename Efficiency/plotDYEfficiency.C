0f557c314782a164c64e0396b268f7feed8f8d8f
Andrius Juodagalvis <andjuo@hotmail.com>
2013-08-28 01:46:29 +0300

    efficiency is saved. Clean-up of the old code

--- Efficiency/plotDYEfficiency.C
+++ Efficiency/plotDYEfficiency.C
@@ -275,8 +275,24 @@ int plotDYEfficiency(const TString conf,
   } // if (processData)
 
 
+  TH2D *hSumPass=createBaseH2("hSumPass","hSumPass",1);
+  TH2D *hSumTotal=createBaseH2("hSumTotal","hSumTotal",1);
+  //TH2D *hSumFail=createBaseH2("hSumFail","hSumFail",1);
+  TH2D *hEff = createBaseH2("hEfficiency","hEff",1);
+
   TString outFileName=inpMgr.correctionFullFileName("efficiency",systMode,0);
   if (DYTools::processData(runMode)) {
+    addHistos(hSumPass,hPass);
+    addHistos(hSumTotal,hTotal);
+    //addHistos(hSumFail,hFail);
+
+    // We need the binomial error for the efficiency:
+    // eff=Pass/Tot, 
+    // (dEff)^2= (1-eff)^2/T^2 (dPass)^2 + eff^2/T^2 (dFail)^2
+    // (dFail)^2 = (dTot)^2 - (dPass)^2
+    hEff->Divide(hSumPass,hSumTotal,1,1,"b");
+
+    // save histograms
     TFile file(outFileName,"recreate");
     int res=1;
     if (res) res=saveVec(file,hPass,"effPassDir");
@@ -286,6 +302,9 @@ int plotDYEfficiency(const TString conf,
     if (res) res=saveVec(file,hMassBinsv,"mass_analysis_bins");
     if (res) res=saveVec(file,hZpeakv,"mass_Zpeak_1GeV");
     if (res) res=saveHisto(file,hSelEvents,"procFileInfo");
+    if (res) res=saveHisto(file,hSumPass,"");
+    if (res) res=saveHisto(file,hSumTotal,"");
+    if (res) res=saveHisto(file,hEff,"");
     if (res) writeBinningArrays(file);
     file.Close();
     if (!res) {
@@ -304,6 +323,9 @@ int plotDYEfficiency(const TString conf,
     if (res) res=loadVec(file,hMassBinsv,"mass_analysis_bins");
     if (res) res=loadVec(file,hZpeakv,"mass_Zpeak_1GeV");
     if (res) res=loadHisto(file,&hSelEvents,"procFileInfo");
+    if (res) res=loadHisto(file,&hSumPass,"");
+    if (res) res=loadHisto(file,&hSumTotal,"");
+    if (res) res=loadHisto(file,&hEff,"");
     file.Close();
     if (!res) {
       std::cout << "error occurred during save to file <" << outFileName << ">\n";
@@ -311,425 +333,30 @@ int plotDYEfficiency(const TString conf,
     }
   }
 
-  TH2D *hSumPass=createBaseH2("hSumPass","hSumPass",1);
-  TH2D *hSumTotal=createBaseH2("hSumTotal","hSumTotal",1);
-  //TH2D *hSumFail=createBaseH2("hSumFail","hSumFail",1);
-  addHistos(hSumPass,hPass);
-  addHistos(hSumTotal,hTotal);
-  //addHistos(hSumFail,hFail);
+  //--------------------------------------------------------------------------------------------------------------
+  // Make plots 
+  //==============================================================================================================  
 
-  TH2D *hEff = createBaseH2("hEff","hEff",1);
-  // We need the binomial error for the efficiency:
-  // eff=Pass/Tot, 
-  // (dEff)^2= (1-eff)^2/T^2 (dPass)^2 + eff^2/T^2 (dFail)^2
-  // (dFail)^2 = (dTot)^2 - (dPass)^2
-  hEff->Divide(hSumPass,hSumTotal,1,1,"b");
-
-  //TH2D *hEffChk=Clone(hEff,
-
+     
+  //--------------------------------------------------------------------------------------------------------------
+  // Summary print out
+  //==============================================================================================================
+  std::cout << std::endl;
+  std::cout << "*" << std::endl;
+  std::cout << "* SUMMARY" << std::endl;
+  std::cout << "*--------------------------------------------------" << std::endl;
+  std::cout << std::endl; 
+  
   printHisto(hSumPass);
   printHisto(hSumTotal);
   printHisto(hEff);
   //printHisto(hSumFail);
 
-    /*
-
-      // The A_FSR starting point: gen level quantities
-      // The FSR subscript means we work with post-FSR generator level quantities
-      double et1 = gen->pt_1;
-      double et2 = gen->pt_2;
-      double eta1 = gen->eta_1;
-      double eta2 = gen->eta_2;
-
-      // Apply acceptance requirement
-      if( ! DYTools::goodEtEtaPair( et1, eta1, et2, eta2 ) ) continue;
-
-      // These events are in acceptance, use them for efficiency denominator
-      Bool_t isBGen1 = DYTools::isBarrel(eta1);
-      Bool_t isBGen2 = DYTools::isBarrel(eta2);
-      // determine number of good vertices
-      pvBr->GetEntry(ientry);
-      int iPUBin=-1;
-      int nGoodPV=-1;
-      double puWeight=1.0;
-	nGoodPV=0;
-	const int new_pv_count_code=1;
-	if (new_pv_count_code) {
-	  nGoodPV=countGoodVertices(pvArr);
-	}
-	else {
-	for (Int_t ipv=0; ipv<pvArr->GetEntriesFast(); ipv++) {
-	  const mithep::TVertex *pv = (mithep::TVertex*)((*pvArr)[ipv]);
-	  if(pv->nTracksFit                        < 1)  continue;
-	  if(pv->ndof                              < 4)  continue;
-	  if(fabs(pv->z)                           > 24) continue;
-	  if(sqrt((pv->x)*(pv->x)+(pv->y)*(pv->y)) > 2)  continue;
-	  nGoodPV++;
-	}
-	}
-      if ((gen->mass>=60) && (gen->mass<=120)) {
-	if (nGoodPV>0) {
-           if (nGoodPV<=nEventsZPeakPURaw.GetNoElements()) 
-	        nEventsZPeakPURaw[nGoodPV] += scale * gen->weight;
-	   iPUBin=DYTools::findPUBin(nGoodPV);
-	   //std::cout << "iPUBin=" << iPUBin << ", nGoodPV=" << nGoodPV << "\n";
-	   if ((iPUBin!=-1) && (iPUBin < nEventsZPeakPU.GetNoElements())) {
-	     nEventsZPeakPU[iPUBin] += scale * gen->weight;
-	   }
-	   //else {
-	   //  std::cout << "error in PU bin indexing iPUBin=" << iPUBin << ", nGoodPV=" << nGoodPV << "\n";
-	   //}
-	}
-	//else std::cout << "nGoodPV=" << nGoodPV << "\n";
-      }
-
-#ifdef usePUReweight
-      puWeight = puReweight.getWeightHildreth(info->nPU);
-      nZv_puUnweighted += scale * gen->weight;
-      nZv_puWeighted += scale * gen->weight * puWeight;
-#endif
-    
-      // Use post-FSR generator level mass for binning
-      int ibinGenM = DYTools::findMassBin(gen->mass);
-      int ibinGenY = DYTools::findAbsYBin(ibinGenM,gen->y);
-      double totalWeight= scale * gen->weight * puWeight;
-      if (useFewzWeights) totalWeight *= fewz.getWeight(gen->vmass,gen->vpt,gen->vy);
-
-      // Accumulate denominator for efficiency calculations
-      if(ibinGenM != -1 && ibinGenY != -1 && ibinGenM < DYTools::nMassBins && ibinGenY < DYTools::nYBins[ibinGenM]){
-	nEventsv(ibinGenM,ibinGenY) += totalWeight;
-        sumWeightsTotaSq(ibinGenM,ibinGenY) += totalWeight*totalWeight;
-	// Split events barrel/endcap using matched supercluster or particle eta
-	if(isBGen1 && isBGen2)                                  { nEventsBBv(ibinGenM,ibinGenY) += totalWeight; } 
-	else if(!isBGen1 && !isBGen2)                           { nEventsEEv(ibinGenM,ibinGenY) += totalWeight; } 
-	else if((isBGen1 && !isBGen2) || (!isBGen1 && isBGen2)) { nEventsBEv(ibinGenM,ibinGenY) += totalWeight; }
-      }else
-        binProblem++;
-
-      // The line below is replaced by the superseeding method, can be cleaned up
-      // if(!(info->triggerBits & eventTriggerBit)) continue;  // no trigger accept? Skip to next event...                                   
-
-      if( !(requiredTriggers.matchEventTriggerBit(info->triggerBits, 
-						  info->runNum))) 
-	continue;
-      
-      // loop through dielectrons
-      dielectronArr->Clear();
-      dielectronBr->GetEntry(ientry);
-
-      for(Int_t i=0; i<dielectronArr->GetEntriesFast(); i++) {
-        const mithep::TDielectron *dielectron = (mithep::TDielectron*)((*dielectronArr)[i]);
-	
-	// Apply selection
-	// Eta cuts and Et cuts
-	if( ! DYTools::goodEtEtaPair( dielectron->scEt_1, dielectron->scEta_1,
-				      dielectron->scEt_2, dielectron->scEta_2 ) ) continue;
-        Bool_t isB1 = DYTools::isBarrel(dielectron->scEta_1);
-        Bool_t isB2 = DYTools::isBarrel(dielectron->scEta_2);
-	
-	if( !( (isB1 == isBGen1 && isB2 == isBGen2 ) 
-	       || (isB1 == isBGen2 && isB2 == isBGen1 ) ) )
-	  countMismatch++;
-
-	// Both electrons must match trigger objects. At least one ordering
-	// must match
-	if( ! requiredTriggers.matchTwoTriggerObjectsAnyOrder( dielectron->hltMatchBits_1,
-							       dielectron->hltMatchBits_2,
-							       info->runNum) ) continue;
-
-	// The clause below can be deleted, it is superseeded by new methods
-//  	if( ! ( 
-//  	       (dielectron->hltMatchBits_1 & leadingTriggerObjectBit && 
-//  		dielectron->hltMatchBits_2 & trailingTriggerObjectBit )
-//  	       ||
-//  	       (dielectron->hltMatchBits_1 & trailingTriggerObjectBit && 
-//  		dielectron->hltMatchBits_2 & leadingTriggerObjectBit ) ) ) continue;
-
-	// *** Smurf ID is superseeded by new selection ***
-// 	// The Smurf electron ID package is the same as used in HWW analysis
-// 	// and contains cuts like VBTF WP80 for pt>20, VBTF WP70 for pt<10
-// 	// with some customization, plus impact parameter cuts dz and dxy
-//  	if(!passSmurf(dielectron)) continue;
-
-	// The selection below is for the EGM working points from spring 2012
-	// recommended for both 2011 and 2012 data
-	if( DYTools::energy8TeV == 1){
-	  if(!passEGMID2012(dielectron, WP_MEDIUM, info->rhoLowEta)) continue;
-	}else{
-	  if(!passEGMID2011(dielectron, WP_MEDIUM, info->rhoLowEta)) continue;
-	}
-
-        // ******** We have a Z candidate! HURRAY! ******** /
-
-	hZMassv[ifile]->Fill(gen->mass,totalWeight);
-
-	// DEBUG
-// 	if(ibinGen == 12)
-// 	  printf("Gen mass %f  reco mass %f  scEt_1= %f  scEt_2= %f  scEta_1= %f  scEta_2= %f\n",
-// 		 gen->mass, dielectron->mass, dielectron->scEt_1, dielectron->scEt_2,
-// 		 dielectron->scEta_1, dielectron->scEta_2);
-	
-	// Accumulate numerator for efficiency calculations
-	if ((nGoodPV>0) && (iPUBin!=-1)) { // -1 may also indicate that the mass was not in Z-peak range
-	  if ((nGoodPV>=0) && (nGoodPV<=nEventsZPeakPURaw.GetNoElements())) nPassZPeakPURaw[nGoodPV] += totalWeight;
-	  if (iPUBin < nPassZPeakPU.GetNoElements()) {
-	    nPassZPeakPU[iPUBin] += totalWeight;
-	  }
-	  //else {
-	  //  std::cout << "error in PU bin indexing\n";
-	  //}
-	}
-	if(ibinGenM != -1 && ibinGenY != -1 && ibinGenM < DYTools::nMassBins && ibinGenY < DYTools::nYBins[ibinGenM]){
-	  nPassv(ibinGenM,ibinGenY) += totalWeight;
-          sumWeightsPassSq(ibinGenM,ibinGenY) += totalWeight*totalWeight;
-	  if(isB1 && isB2)                            { nPassBBv(ibinGenM,ibinGenY) += totalWeight; } 
-	  else if(!isB1 && !isB2)                     { nPassEEv(ibinGenM,ibinGenY) += totalWeight; } 
-	  else if((isB1 && !isB2) || (!isB1 && isB2)) { nPassBEv(ibinGenM,ibinGenY) += totalWeight; }
-	}
-
-      } // end loop over dielectrons
-    } // end loop over events 
-    delete infile;
-    infile=0, eventTree=0;
-  } // end loop over files
-  delete gen;
-
-  cout << "ERROR: binning problem (" << binProblem <<" events in ECAL gap)"<<endl;
-
-  effv      = 0;
-  effErrv   = 0;
-  effBBv    = 0;
-  effErrBBv = 0;
-  effBEv    = 0;
-  effErrBEv = 0;
-  effEEv    = 0;
-  effErrEEv = 0;
-  for(int i=0; i<DYTools::nMassBins; i++)
-    for(int j=0; j<DYTools::nYBins[i]; j++){
-      if(nEventsv(i,j) != 0){
-        double nPass, nFail, nPassErr, nFailErr;
-        nPass=nPassv(i,j);
-        nFail=nEventsv(i,j)-nPassv(i,j); 
-        nPassErr=sqrt(sumWeightsPassSq(i,j));
-        nFailErr=sqrt(sumWeightsTotaSq(i,j)-sumWeightsPassSq(i,j));
-        effv(i,j) = nPassv(i,j)/nEventsv(i,j);
-        //effErrv(i,j) = sqrt(effv(i,j)*(1-effv(i,j))/nEventsv(i,j));
-        effErrv(i,j) = sqrt(( nFail*nFail * nPassErr*nPassErr + nPass*nPass * nFailErr*nFailErr)) / (nEventsv(i,j)*nEventsv(i,j));
-      }
-    
-      if (nEventsBBv(i,j) != 0) {
-        effBBv(i,j) = nPassBBv(i,j)/nEventsBBv(i,j);
-        effErrBBv(i,j) = sqrt(effBBv(i,j)*(1-effBBv(i,j))/nEventsBBv(i,j));
-      }
-
-      if (nEventsBEv(i,j) != 0) {
-        effBEv(i,j) = nPassBEv(i,j)/nEventsBEv(i,j);
-        effErrBEv(i,j) = sqrt(effBEv(i,j)*(1-effBEv(i,j))/nEventsBEv(i,j));
-      }
-      
-      if (nEventsEEv(i,j) != 0) {
-        effEEv(i,j) = nPassEEv(i,j)/nEventsEEv(i,j);
-        effErrEEv(i,j) = sqrt(effEEv(i,j)*(1-effEEv(i,j))/nEventsEEv(i,j));
-      }
-    };
-
-  effZPeakPU=0; effErrZPeakPU=0;
-  for (int i=0; i<DYTools::nPVBinCount; ++i) {
-    effZPeakPU[i]= nPassZPeakPU[i]/nEventsZPeakPU[i];
-    effErrZPeakPU[i]= sqrt(effZPeakPU[i]*(1-effZPeakPU[i])/nEventsZPeakPU[i]);
-  }
-
-  printf("Sanity check: gen vs reco barrel-endcap assignment mismatches: %d\n",countMismatch);
-
-  //--------------------------------------------------------------------------------------------------------------
-  // Make plots 
-  //==============================================================================================================  
-
-  // destination dir
-  TString extraTag= DYTools::analysisTag;
-  if (!useFewzWeights) extraTag.Append("_noFEWZ");
-  CPlot::sOutDir="plots" + extraTag;
-  TString outputDir(TString("../root_files/constants/")+dirTag);
-  gSystem->mkdir(outputDir,kTRUE);
-  TString fnamePlots=outputDir + TString("/event_efficiency_plots") + extraTag + TString(".root");
-  TFile *filePlots=new TFile(fnamePlots,"recreate");
-  if (!filePlots || !filePlots->IsOpen()) {
-    std::cout << "failed to create a file <" << fnamePlots << ">\n";
-    throw 2;
-  }
-
-  TCanvas *c = MakeCanvas("canvEfficiency","canvEfficiency",800,600);
-
-  // string buffers
-  char ylabel[50];   // y-axis label
-
-  // Z mass
-  sprintf(ylabel,"a.u. / %.1f GeV/c^{2}",hZMassv[0]->GetBinWidth(1));
-  CPlot plotZMass1("zmass1","","m(Z) [GeV/c^{2}]",ylabel);
-  for(UInt_t i=0; i<fnamev.size(); i++) { 
-    plotZMass1.SetYRange(1, 10000000);
-    plotZMass1.AddHist1D(hZMassv[i],labelv[i],"hist",colorv[i],linev[i]); 
-  }
-  plotZMass1.SetLogy();
-  plotZMass1.Draw(c);
-  SaveCanvas(c, "zmass1");
-
-  PlotMatrixVariousBinning(effv, "efficiency", "LEGO2", filePlots);
-  filePlots->Close();
-  if (DYTools::study2D==0)
-    Plot1D(effv,effErrv,"efficiency1D","efficiency");
-
-  // Store constants in the file
-  //TString effConstFileName(outputDir+TString("/event_efficiency_constants.root"));
-  TString effConstFileName(outputDir+TString("/event_efficiency_constants") + extraTag + TString(".root"));
-
-  TMatrixD nEventsvErr=nEventsv;
-  TMatrixD nEventsBBvErr=nEventsBBv;
-  TMatrixD nEventsBEvErr=nEventsBEv;
-  TMatrixD nEventsEEvErr=nEventsEEv;
-  TMatrixD nPassBBvErr=nPassBBv;
-  TMatrixD nPassBEvErr=nPassBEv;
-  TMatrixD nPassEEvErr=nPassEEv;
-
-  TMatrixD nPassvErr=nPassv;
-  for (int i=0; i<DYTools::nMassBins; ++i) {
-    for (int j=0; j<DYTools::nYBins[i]; ++j) {
-      nEventsvErr(i,j)=sqrt(sumWeightsTotaSq(i,j));
-    }
-  }
-  for (int i=0; i<DYTools::nMassBins; ++i) {
-    for (int j=0; j<DYTools::nYBins[i]; ++j) {
-      nPassvErr(i,j)=sqrt(sumWeightsPassSq(i,j));
-    }
-  }
-  nEventsBBvErr=0;
-  nEventsBEvErr=0;
-  nEventsEEvErr=0;
-  nPassBBvErr=0;
-  nPassBEvErr=0;
-  nPassEEvErr=0;
-
-   TFile fa(effConstFileName,"recreate");
-   effv.Write("efficiencyArray");
-   effErrv.Write("efficiencyErrArray");
-
-   /
-   nPassv.Write("effEval_nPass");
-   nPassvErr.Write("effEval_nPassErr");
-   nEventsv.Write("effEval_nTotal");
-   nEventsvErr.Write("effEval_nTotalErr");
-
-   nEventsBBv.Write("effEval_nTotalBB");
-   nEventsBBvErr.Write("effEval_nTotalBBErr");
-   nEventsBEv.Write("effEval_nTotalBE");
-   nEventsBEvErr.Write("effEval_nTotalBEErr");
-   nEventsEEv.Write("effEval_nTotalEE");
-   nEventsEEvErr.Write("effEval_nTotalEEErr");
-   nPassBBv.Write("effEval_nPassBB");
-   nPassBBvErr.Write("effEval_nPassBBErr");
-   nPassBEv.Write("effEval_nPassBE");
-   nPassBEvErr.Write("effEval_nPassBEErr");
-   nPassEEv.Write("effEval_nPassEE");
-   nPassEEvErr.Write("effEval_nPassEEErr");
-
-   effBBv.Write("efficiencyBB");
-   effErrBBv.Write("efficiencyBBErr");
-   effBEv.Write("efficiencyBE");
-   effErrBEv.Write("efficiencyBEErr");
-   effEEv.Write("efficiencyEE");
-   effErrEEv.Write("efficiencyEEErr");
-   /
-   //effZPeakPU.Write("efficiencyZPeakPUArray");
-   //effErrZPeakPU.Write("efficiencyErrZPeakPUArray");
-   //nEventsZPeakPU.Write("nEventsZPeakPUArray");
-   //nPassZPeakPU.Write("nPassZPeakPUArray");
-   //nEventsZPeakPURaw.Write("nEventsZPeakPURawArray");
-   //nPassZPeakPURaw.Write("nPassZPeakPURawArray");
-   //unfolding::writeBinningArrays(fa);
-   fa.Close();
-     
-  //--------------------------------------------------------------------------------------------------------------
-  // Summary print out
-  //==============================================================================================================
-  cout << endl;
-  cout << "*" << endl;
-  cout << "* SUMMARY" << endl;
-  cout << "*--------------------------------------------------" << endl;
-  cout << endl; 
-  
-  cout << labelv[0] << " file: " << fnamev[0] << endl;
-  printf("     Number of generated events: %8.1lf\n",nZv);
-#ifdef usePUReweight
-  printf("     Number of selected events (puUnweighted): %8.1lf\n",nZv_puUnweighted);
-  printf("     Number of selected events (puWeighted)  : %8.1lf\n",nZv_puWeighted);
-#endif
-
-  const char *yRangeStr=(DYTools::study2D) ? "rapidity range" : "";
-  printf(" mass range  %s preselected      passed     total_Eff        BB-BB_Eff        EB-BB_Eff        EB-EB_Eff\n",yRangeStr);
-  for(int i=0; i<DYTools::nMassBins; i++){
-    double *rapidityBinLimits=DYTools::getYBinLimits(i);
-    for (int yi=0; yi<DYTools::nYBins[i]; ++yi) {
-      printf(" %4.0f-%4.0f ", DYTools::massBinLimits[i], DYTools::massBinLimits[i+1]);
-      if (DYTools::study2D!=0) printf(" %4.2f-%4.2f ", rapidityBinLimits[yi], rapidityBinLimits[yi+1]);
-      printf("    %10.0f   %10.0f   %7.4f+-%6.4f  %7.4f+-%6.4f  %7.4f+-%6.4f  %7.4f+-%6.4f \n",
-	     nEventsv(i,yi), nPassv(i,yi),
-	     effv(i,yi), effErrv(i,yi),
-	     effBBv(i,yi), effErrBBv(i,yi),
-	     effBEv(i,yi), effErrBEv(i,yi),
-	     effEEv(i,yi), effErrEEv(i,yi));
-    }
-    delete rapidityBinLimits;
-  }
-
-  if (1) {
-    printf(" mass_range  %s total_preselected  total_passed     BB-BB_preselected  passed    EB-BB_preselected  passed        EB-EB_preselected  passed\n",yRangeStr);
-    for(int i=0; i<DYTools::nMassBins; i++){
-      double *rapidityBinLimits=DYTools::getYBinLimits(i);
-      for (int yi=0; yi<DYTools::nYBins[i]; ++yi) {
-	printf(" %4.0f-%4.0f ", DYTools::massBinLimits[i], DYTools::massBinLimits[i+1]);
-	if (DYTools::study2D!=0) printf(" %4.2f-%4.2f ", rapidityBinLimits[yi], rapidityBinLimits[yi+1]);
-	printf("    %8.1f %8.1f   %8.1f %8.1f  %8.1f %8.1f  %8.1f %8.1f\n",
-	       nEventsv(i,yi), nPassv(i,yi),
-	       nEventsBBv(i,yi), nPassBBv(i,yi),
-	       nEventsBEv(i,yi), nPassBEv(i,yi),
-	       nEventsEEv(i,yi), nPassEEv(i,yi));
-      }
-      delete rapidityBinLimits;
-    }
-  }
-
-  //printout to the txtFile in latex format
-  if (DYTools::study2D)
-    {
-      latexPrintoutEfficiency2D(effv,effErrv,"Efficiency/plotDYEfficiency.C");
-    }
-  else if (DYTools::study2D==0)
-    {
-      latexPrintoutEfficiency1D(effv,effErrv,"Efficiency/plotDYEfficiency.C");
-    }
-
-  cout << endl;
-  std::cout<<"printout in the Latex format is saved to the text file"<<std::endl;
-
-  printf("\n\nZ-peak efficiency\n");
-  printf(" PU bin    preselected      passed     total_Eff\n");
-  for(int i=0; i<DYTools::nPVBinCount; i++){
-    printf(" %4.0f-%4.0f   %10.0f   %10.0f   %7.4f+-%6.4f\n",
-	   DYTools::nPVLimits[i], DYTools::nPVLimits[i+1],
-	   nEventsZPeakPU[i], nPassZPeakPU[i],
-	   effZPeakPU[i], effErrZPeakPU[i]);
-  }
-
-  //sanity check printout
-  printSanityCheck(effv, effErrv, "eff");
-
-  cout << endl;
-*/  
   gBenchmark->Show("plotDYEfficiency");
   return retCodeOk;
 }
 
 
-//=== FUNCTION DEFINITIONS ======================================================================================
+//=== FUNCTION IMPLEMENTATIONS ======================================================================================
 
 //--------------------------------------------------------------------------------------------------
