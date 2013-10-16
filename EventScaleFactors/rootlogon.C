c2daa65ebadb24a945be7fdd4b0bad1f9b6a269b
Andrius Juodagalvis <andjuo@hotmail.com>
2013-10-16 14:08:55 +0300

    added optional linking with calcEventEffLink

--- EventScaleFactors/rootlogon.C
+++ EventScaleFactors/rootlogon.C
@@ -7,5 +7,14 @@
   gROOT->ProcessLine(".L fitFunctionsCore.cc+");
   gROOT->ProcessLine(".L fitFunctions.cc+");
   gROOT->ProcessLine(".L tnpSelectEvents.hh+");
+
+  //
+  // Some compare*C macros need calcEventEffLink.cc
+  //
+  if (0) {
+    std::cout << "\n\n\t\tlinking calcEventEffLink.cc\n\n";
+    gROOT->ProcessLine(".L calcEventEffLink.cc+");
+  }
+
 }
 
