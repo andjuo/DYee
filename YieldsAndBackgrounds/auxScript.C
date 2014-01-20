5b16edc943de6005e83604aae2e0d14cd596605a
Andrius Juodagalvis <andjuo@hotmail.com>
2014-01-20 17:01:32 +0200

    updated auxScript files

--- YieldsAndBackgrounds/auxScript.C
+++ YieldsAndBackgrounds/auxScript.C
@@ -1,8 +1,8 @@
 {
   gROOT->ProcessLine(".L prepareYields.C+");
-  //prepareYields("../config_files/data_vilnius8TeV.conf.py",DYTools::NORMAL_RUN,DYTools::NO_SYST);
-  prepareYields("../config_files/data_vilnius8TeV.conf.py",DYTools::DEBUG_RUN,DYTools::NO_SYST);
+  prepareYields("../config_files/data_vilnius8TeV_reg.conf.py",DYTools::NORMAL_RUN,DYTools::NO_SYST);
+  //prepareYields("../config_files/data_vilnius8TeV_reg.conf.py",DYTools::DEBUG_RUN,DYTools::NO_SYST);
 
   gROOT->ProcessLine(".L subtractBackground.C+");
-  subtractBackground("../config_files/data_vilnius8TeV.conf.py",DYTools::DEBUG_RUN,DYTools::NO_SYST);
+  subtractBackground("../config_files/data_vilnius8TeV_reg.conf.py",DYTools::NORMAL_RUN,DYTools::NO_SYST);
 }
