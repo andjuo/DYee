b65a0f2ae1fe03e2667505b4710b84081c0c2c36
Andrius Juodagalvis <andjuo@hotmail.com>
2014-01-25 15:10:07 +0200

    added implementation fillEfficiencyConstants(inpMgr) to match the updated calcEventEff.C

--- EventScaleFactors/calcEventEffLink.cc
+++ EventScaleFactors/calcEventEffLink.cc
@@ -1,3 +1,8 @@
 #include "calcEventEffLink.h"
 
 #include "calcEventEff.C"
+
+int fillEfficiencyConstants( const InputFileMgr_t &inpMgr ) {
+  DYTools::TSystematicsStudy_t systMode=DYTools::NO_SYST;
+  return fillEfficiencyConstants(inpMgr,systMode);
+}
