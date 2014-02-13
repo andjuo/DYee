fcf4ec38450405d9c35cddae2214426e5e28506f
Andrius Juodagalvis <andjuo@hotmail.com>
2014-02-13 13:20:38 +0200

    added a possibility to save scale factors to a file

--- Covariance/plotEffs.C
+++ Covariance/plotEffs.C
@@ -1,12 +1,22 @@
 #include "CovariantEff.h"
 
+int extractOldSF=0;
+
 void plotEffs(int debugMode) {
   TString confFileName="../config_files/data_vilnius8TeV_regSSD.conf.py";
   std::cout << "debugMode=" << debugMode << " is ignored\n";
 
+  if (extractOldSF) {
+    confFileName="../config_files/data_DMDY8TeV_oldNtuples.conf.py";
+  }
+
   //DYTools::TSystematicsStudy_t systMode=DYTools::NO_SYST;
   CovariantEffMgr_t mgr;
   int nExps=1;
+
+  if (extractOldSF) {
+    mgr.editMgr().rootFileBaseDir("../../DMDY-Ilya-20130808-my-copy/root_files/");
+  }
 
   HERE("calling setup");
   assert(mgr.Setup(confFileName,nExps));
@@ -14,9 +24,14 @@ void plotEffs(int debugMode) {
 
   std::cout << "\n\nok. Start studies\n";
 
-  TString plotFName="effs.root";
+  TString tnpString=TString(mgr.mgr().getTNP_etetaBinningString().c_str());
+  tnpString.ReplaceAll(" ","");
+  TString plotFName=Form("el-effs-%s.root",tnpString.Data());
+  if (extractOldSF) plotFName.ReplaceAll(".root","-summer2013.root");
   TFile faPlots(plotFName,"recreate");
   drawEfficiencies(&faPlots);
+  drawScaleFactors(&faPlots,1); // save arrays
   faPlots.Close();
+  std::cout << "file " << faPlots.GetName() << " saved.\n";
 }
 
