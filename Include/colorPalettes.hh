99b48b722abdc03f9ad97ccfee6544e52aa4d7a3
Andrius Juodagalvis <andjuo@hotmail.com>
2014-02-20 12:48:10 +0200

    Include/colorPalettes.hh Include/MitStyleRemix.hh: added function SetSideSpaces to control sides of a pad

--- Include/colorPalettes.hh
+++ Include/colorPalettes.hh
@@ -301,5 +301,29 @@ void AdjustFor2DplotWithHeight(TCanvas *c, double rmargin=0.18) {
 
 // ------------------------------------------------------------
 
+// A function to control space that histogram takes.
+// E.g. if the y-labels+title need more space call SetSideSpaces(c,0.2,0,0,0);
+
+#ifndef MitStyleRemix_HH
+inline
+void SetSideSpaces(TCanvas *c, double dxLeft=0.05, double dxRight=0.02, double dyTop=0., double dyBottom=0.02) {
+  int count=0;
+  for (int i=0; i<50; i++) {
+    TPad *pad=(TPad*)c->GetPad(i);
+    if (pad) {
+      count++;
+      pad->Range(pad->GetX1()-dxLeft,pad->GetY1()-dyBottom,
+		 pad->GetX2()-dxRight,pad->GetY2()-dyTop);
+      pad->SetLeftMargin(pad->GetLeftMargin() + dxLeft);
+      pad->SetRightMargin(pad->GetRightMargin() + dxRight);
+      pad->SetTopMargin(pad->GetTopMargin() + dyTop);
+      pad->SetBottomMargin(pad->GetBottomMargin() + dyBottom);
+    }
+  }
+}
+#endif
+
+// ------------------------------------------------------------
+
 
 #endif
