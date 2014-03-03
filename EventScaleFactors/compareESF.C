a9dbbeb44839e73467842b17f513c2b19ffcbf31
Andrius Juodagalvis <andjuo@hotmail.com>
2014-03-03 17:02:40 +0200

    EventScaleFactors/compareESF.C: more cases

--- EventScaleFactors/compareESF.C
+++ EventScaleFactors/compareESF.C
@@ -63,11 +63,28 @@ TH2D* loadESF_2D_7TeV(TString fname, TString label) {
 
 // ------------------------------------------------------------
 
+TMatrixD* enforceYRanges(int the_case) {
+  TMatrixD* r=new TMatrixD(6,2);
+  if (the_case==1) {
+    (*r)(0,0)=0.85; (*r)(0,1)=1.15;
+    (*r)(1,0)=0.85; (*r)(1,1)=1.10;
+    (*r)(2,0)=0.89; (*r)(2,1)=1.11;
+    (*r)(3,0)=0.94; (*r)(3,1)=1.10;
+    (*r)(4,0)=0.95; (*r)(4,1)=1.12;
+    (*r)(5,0)=0.95; (*r)(5,1)=1.12;
+  }
+  return r;
+}
 
-void compareESF(TString esfLongStr1="1D_Full2012_hltEffOld_PU",
-		TString esfLongStr2="1D",
+// ------------------------------------------------------------
+
+
+void compareESF(int iBr=0,
 		int nDim=1,
-		int doSave=1) {
+		int doSave=0) {
+
+  TString esfLongStr1="1D_Full2012_hltEffOld_PU";
+  TString esfLongStr2="1D";
   TString path1="/home/andriusj/cms/DYee8TeV-20140118/root_files/constants/DY_j22_19712pb/";
   TString path2="/home/andriusj/cms/DYee-20131024/root_files_reg/constants/DY_j22_19712pb/";
   //path2="/home/andriusj/cms/CMSSW_3_8_4/src/DYee8TeV-20130801/DrellYanDMDY/root_files/tag_and_probe/DY_j22_19789pb/";
@@ -97,6 +114,11 @@ void compareESF(TString esfLongStr1="1D_Full2012_hltEffOld_PU",
   int secondIs7TeV=0;
   double set_ratio_y_min=0.96;
   double set_ratio_y_max=1.04;
+
+  int compSet=-1;
+  int swapColors=0;
+
+  TMatrixD *setYRanges2D=NULL;
 
   /*
   if (0) {
@@ -200,7 +222,7 @@ void compareESF(TString esfLongStr1="1D_Full2012_hltEffOld_PU",
     saveDirTag="-diffEtaMax-esf--";
   }
 
-  if (1) {
+  if (0) {
     path1="/home/andriusj/cms/CMSSW_3_8_4/src/DYee8TeV-20130801/DrellYanDMDY/root_files/constants/DY_j22_19789pb/";
     //path1="/home/andriusj/cms/DYee8TeV-20140118/root_files/constants/DY_j22_19712pb/"; // new n-tuples
     path2="/home/andriusj/cms/DYee-20131024/root_files_reg/constants/DY_j22_19712pb/"; 
@@ -214,11 +236,152 @@ void compareESF(TString esfLongStr1="1D_Full2012_hltEffOld_PU",
     saveDirTag="-diffEtaMax-esf--";
   }
 
+  if (0) { // added 2014.02.19
+    path1="/home/andriusj/cms/DYee-20140217-EESF/root_files_reg/constants/DY_j22_19712pb/";
+    path2="/home/andriusj/cms/DYee-20140217-EESF/root_files_reg/constants/DY_j22_19712pb_egamma/"; 
+    esfLongStr1="ourSF_asymHLT_1D";
+    esfLongStr2="egamma_asymHLT_1D";
+    label1="our SF";
+    label2="EGamma";
+    if (nDim==2) { set_ratio_y_min=0.95; set_ratio_y_max=1.05; }
+    else { set_ratio_y_min=0.98; set_ratio_y_max=1.05; }
+    fnameTag="-egamma";
+    saveDirTag="-egamma--";
+  }
+
+  if (0) { // added 2014.02.26
+    path1="../Covariance/";
+    path2="../Covariance/";
+    fnameBase="covRhoFileSF";
+    esfLongStr1="_nMB41_egamma_asymHLT_Unregressed_energy-allSyst_100";
+    esfLongStr2="_nMB41_egamma_asymHLT_Unregressed_energy-noSyst_100";
+    label1="EGamma (unreg.en.+all syst.)";
+    label2="EGamma (unreg.en.)";
+    fnameTag="-egamma-unregEn-allSyst";
+    saveDirTag="-egamma-unregEn--";
+    if (nDim==2) { set_ratio_y_min=0.95; set_ratio_y_max=1.05; }
+    else { set_ratio_y_min=0.94; set_ratio_y_max=1.06; }
+    compSet=10;
+    swapColors=0;
+  }
+
+  if (0) { // added 2014.03.01
+    path1="../Covariance/";
+    path2="../Covariance/";
+    fnameBase="covRhoFileSF";
+    esfLongStr1="_nMB41_egamma_asymHLT_Unregressed_energy-allSyst_100";
+    esfLongStr2="_nMB41_egamma_asymHLT_Unregressed_energy-allSyst-egammaSystOnly_100";
+    label1="EGamma (unreg.en.+all syst.)";
+    label2="EGamma (unreg.en.+EG(reco,id) +HLT)";
+    fnameTag="-egamma-unregEn-allSyst-egammaSystOnly";
+    saveDirTag="-egamma-unregEn--";
+    if (nDim==2) { set_ratio_y_min=0.95; set_ratio_y_max=1.05; }
+    else { set_ratio_y_min=0.93; set_ratio_y_max=1.07; }
+    compSet=13;
+    swapColors=0;
+    setYRanges2D=enforceYRanges(1);
+  }
+
+  if (1) { // added 2014.03.03
+    path1="../Covariance/";
+    path2="../Covariance/";
+    fnameBase="covRhoFileSF";
+    esfLongStr1="_nMB41_egamma_asymHLT_Unregressed_energy-recoSyst_100";
+    esfLongStr2="_nMB41_egamma_asymHLT_Unregressed_energy-recoSyst-egammaSystOnly_100";
+    label1="EGamma (unreg.en.+reco syst.)";
+    label2="EGamma (unreg.en.+EG(reco))";
+    fnameTag="-egamma-unregEn-recoSyst-egammaSystOnly";
+    saveDirTag="-egamma-unregEn--";
+    if (nDim==2) { set_ratio_y_min=0.95; set_ratio_y_max=1.05; }
+    else { set_ratio_y_min=0.93; set_ratio_y_max=1.07; }
+    compSet=12;
+    swapColors=0;
+    setYRanges2D=enforceYRanges(1);
+  }
+
+  if (0) { // added 2014.02.27
+    path1="../Covariance/";
+    path2="../Covariance/";
+    fnameBase="covRhoFileSF";
+    esfLongStr1="_nMB41_egamma_asymHLT_regEn-allSyst_100";
+    esfLongStr2="_nMB41_egamma_asymHLT_regEn_100";
+    label1="EGamma (reg.en.+all syst.)";
+    label2="EGamma (reg.en.)";
+    fnameTag="-egamma-regEn-allSyst";
+    saveDirTag="-egamma-regEn--";
+    if (nDim==2) { set_ratio_y_min=0.95; set_ratio_y_max=1.05; }
+    else { set_ratio_y_min=0.94; set_ratio_y_max=1.06; }
+    compSet=10;
+  }
+
+  if (0) { // added 2014.02.28
+    path1="../Covariance/";
+    path2="../Covariance/";
+    fnameBase="covRhoFileSF";
+    esfLongStr1="_nMB41_egamma_asymHLT_regEn-allSyst_100";
+    esfLongStr2="_nMB41_egamma_asymHLT_Unregressed_energy-allSyst_100";
+    label1="EGamma (reg.en.+all syst.)";
+    label2="EGamma (unreg.en.+all syst.)";
+    fnameTag="-egamma-cmpEn-allSyst";
+    saveDirTag="-egamma-cmpEn--";
+    if (nDim==2) { set_ratio_y_min=0.95; set_ratio_y_max=1.05; }
+    else { set_ratio_y_min=0.94; set_ratio_y_max=1.06; }
+    compSet=11;
+    swapColors=1;
+  }
 
 
   if (DYTools::study2D) {
     esfLongStr1.ReplaceAll("1D","2D");
     esfLongStr2.ReplaceAll("1D","2D");
+    esfLongStr1.ReplaceAll("nMB41","nMB7");
+    esfLongStr2.ReplaceAll("nMB41","nMB7");
+  }
+
+  if (iBr!=0) {
+    if (compSet==10) {
+      if (iBr==1) {
+	esfLongStr1.ReplaceAll("allSyst","recoSyst");
+	label1.ReplaceAll("all syst","reco syst");
+	fnameTag.ReplaceAll("allSyst","recoSyst");
+      }
+      else if (iBr==2) {
+	esfLongStr1.ReplaceAll("allSyst","idSyst");
+	label1.ReplaceAll("all syst","ID syst");
+	fnameTag.ReplaceAll("allSyst","idSyst");
+      }
+      else if (iBr==3) {
+	esfLongStr1.ReplaceAll("allSyst","hltSyst");
+	label1.ReplaceAll("all syst","HLT syst");
+	fnameTag.ReplaceAll("allSyst","hltSyst");
+      }
+      else if (iBr==-1) {
+	esfLongStr2.ReplaceAll("-noSyst_100","-recoSyst_100");
+	label2.ReplaceAll("en.","en.+reco syst");
+	fnameTag.ReplaceAll("allSyst","allSyst_vsRecoSyst");
+      }
+      else if (iBr==-2) {
+	esfLongStr2.ReplaceAll("-noSyst_100","-idSyst_100");
+	label2.ReplaceAll("en.","en.+ID syst");
+	fnameTag.ReplaceAll("allSyst","allSyst_vsIDSyst");
+      }
+      else if (iBr==-3) {
+	esfLongStr2.ReplaceAll("-noSyst_100","-hltSyst_100");
+	label2.ReplaceAll("en.","en.+HLT syst");
+	fnameTag.ReplaceAll("allSyst","allSyst_vsHLTSyst");
+      }
+    }
+    else if (compSet==12) {
+      if (iBr==0) {
+      }
+      else if (iBr==1) {
+	esfLongStr1.ReplaceAll("reco","id");
+	esfLongStr2.ReplaceAll("reco","id");
+	label1.ReplaceAll("reco","id");
+	label2.ReplaceAll("reco","id");
+	fnameTag.ReplaceAll("reco","id");
+      }
+    }
   }
 
   TString fname1=path1 + fnameBase + esfLongStr1 + TString(".root");
@@ -281,24 +444,37 @@ void compareESF(TString esfLongStr1="1D_Full2012_hltEffOld_PU",
     std::vector<ComparisonPlot_t*> cpV;
     for (int i=0; i<6; ++i) {
       TString mRange=Form("M_%2.0lf_%2.0lf",DYTools::massBinLimits[i+1],DYTools::massBinLimits[i+2]);
-      ComparisonPlot_t *cp=new ComparisonPlot_t(ComparisonPlot_t::_ratioPlain,Form("cp_%s",mRange.Data()),mRange,"|y|","event scale factor","ratio");
+      ComparisonPlot_t *cp=NULL;
+      //cp=new ComparisonPlot_t(ComparisonPlot_t::_ratioRel,Form("cp_%s",mRange.Data()),mRange,"|y|","event scale factor","rel.ratio");
+      cp=new ComparisonPlot_t(ComparisonPlot_t::_ratioPlain,Form("cp_%s",mRange.Data()),mRange,"|y|","event scale factor","ratio");
       cp->SetRatioNdivisions(404);
       cp->SetYTitleSize(0.08,0.97);
       cp->SetYLabelSize(0.06);
       cp->SetRatioYTitleSize(0.18,0.40);
 
+      if (setYRanges2D) {
+	cp->SetYRange((*setYRanges2D)(i,0),(*setYRanges2D)(i,1));
+      }
       //cp->SetRatioYRange(0.97,1.01);
       cp->SetRatioYRange(set_ratio_y_min,set_ratio_y_max);
+      //cp->SetRatioYRange(-0.01,0.01);
       cp->AddHist1D(hProfEsf1[i],label1,"LPE1",kBlack,1,0);
       cp->AddHist1D(hProfEsf2[i],label2,"PE3",kBlue ,2,0);
       //cp->AddHist1D(hProfEsf2[i],label2,"LPE3",kBlue ,2,0);
+      cp->AddLine(0.,1.0, 2.4,1.0, kBlue+1,kDashed);
       cpV.push_back(cp);
     }
 
     cy=new TCanvas("cy","cy",1200,800);
     cpV[0]->Prepare6Pads(cy, 1);
+
+    int printRatio=0;
     
     for (int i=0; i<6; ++i) {
+      if (printRatio) {
+	std::cout << "---- " << cpV[i]->GetTitle() << "\n";
+	cpV[i]->SetPrintRatio(1);
+      }
       cpV[i]->Draw6(cy,1,i+1);
       cpV[i]->TransLegend(-0.42,-0.1);
       cpV[i]->WidenLegend(0.1,0.1);
@@ -315,17 +491,27 @@ void compareESF(TString esfLongStr1="1D_Full2012_hltEffOld_PU",
     //h2esf2->Print("range");
     //h2->Print("range");
 
-    ComparisonPlot_t *cp=new ComparisonPlot_t(ComparisonPlot_t::_ratioPlain,"cpESF","","M_{ee} [GeV]", "event scale factor","ratio");
+    int relRatio=0;
+    ComparisonPlot_t *cp=NULL;
+    if (!relRatio) cp=new ComparisonPlot_t(ComparisonPlot_t::_ratioPlain,"cpESF","","M_{ee} [GeV]", "event scale factor","ratio");
+    else cp=new ComparisonPlot_t(ComparisonPlot_t::_ratioRel,"cpESF","","M_{ee} [GeV]", "event scale factor","rel.ratio");
     cp->SetYTitleSize(0.07,1.16);
     cp->SetRatioYTitleSize(0.17,0.47);
     cp->SetPrintRatio(1);
 
+    int color1=kBlack;
+    int color2=46; // red-brown
+    if (swapColors) { int icol=color1; color1=color2; color2=icol; }
+
     //cp->SetRatioYRangeC(1,0.04);
     cp->SetRatioYRange(set_ratio_y_min, set_ratio_y_max);
+    if (relRatio) cp->SetRatioYRangeC(0.,0.01);
     cp->SetLogx();
     //h1->GetYaxis()->SetTitleOffset(1.47);
-    cp->AddHist1D(h1, label1,"LPE1",kBlack,1,0);
-    cp->AddHist1D(h2, label2,"LPE1",kBlue ,2,0);
+    cp->AddHist1D(h1, label1,"LPE1",color1,1,0);
+    cp->AddHist1D(h2, label2,"LPE1",color2,1,0);
+
+    cp->AddLine(DYTools::massBinLimits[0],1.,DYTools::massBinLimits[DYTools::nMassBins],1., kBlack,kDashed);
 
     cy=new TCanvas("cy","cy",600,700);
     cp->Prepare2Pads(cy);
