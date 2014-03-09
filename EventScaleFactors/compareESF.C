d9663c5e7169c0df6bdaedd692ba7ba3b6aa0b80
Andrius Juodagalvis <andjuo@hotmail.com>
2014-03-09 16:04:16 +0200

    EventScaleFactors/compareESF.C: modified script to include up to 4 different sets

--- EventScaleFactors/compareESF.C
+++ EventScaleFactors/compareESF.C
@@ -85,8 +85,10 @@ void compareESF(int iBr=0,
 
   TString esfLongStr1="1D_Full2012_hltEffOld_PU";
   TString esfLongStr2="1D";
+  TString esfLongStr3,esfLongStr4;
   TString path1="/home/andriusj/cms/DYee8TeV-20140118/root_files/constants/DY_j22_19712pb/";
   TString path2="/home/andriusj/cms/DYee-20131024/root_files_reg/constants/DY_j22_19712pb/";
+  TString path3,path4;
   //path2="/home/andriusj/cms/CMSSW_3_8_4/src/DYee8TeV-20130801/DrellYanDMDY/root_files/tag_and_probe/DY_j22_19789pb/";
   //path2="/home/andriusj/cms/DYee8TeV-20140118-maxEta24/root_files/tag_and_probe/DY_j22_19712pb/";
 
@@ -97,7 +99,9 @@ void compareESF(int iBr=0,
   }
 
 
-  TString fnameBase="scale_factors_";
+  TString fnameBase1="scale_factors_";
+  TString fnameBase2="scale_factors_";
+  TString fnameBase3, fnameBase4;
 
   TString dimStr;
   if (nDim==1) dimStr.Append("1D");
@@ -111,6 +115,7 @@ void compareESF(int iBr=0,
   TString saveDirTag,fnameTag;
   TString label1="DrellYanDMDY";
   TString label2="DYee";
+  TString label3,label4;
   int secondIs7TeV=0;
   double set_ratio_y_min=0.96;
   double set_ratio_y_max=1.04;
@@ -119,30 +124,6 @@ void compareESF(int iBr=0,
   int swapColors=0;
 
   TMatrixD *setYRanges2D=NULL;
-
-  /*
-  if (0) {
-    path1="/home/andriusj/cms/DYee8TeV-20140118/root_files/constants/DY_j22_19712pb/";
-    path2="/home/andriusj/cms/DYee8TeV-20140118-maxEta24/root_files/constants/DY_j22_19712pb/";
-    esfLongStr1="1D_Full2012_hltEffOld_PU";
-    esfLongStr2="1D_Full2012_hltEffOld_PU";
-    label1="DMDY |#eta|<2.5";
-    label2="DMDY |#eta|<2.4";
-    fnameTag="-DMDY";
-    saveDirTag="-diffEtaMax-esf--";
-  }
-
-  if (0) {
-    path1="/home/andriusj/cms/DYee-20131024/root_files_reg/constants/DY_j22_19712pb/"; 
-    path2=path1;
-    esfLongStr1="1D";
-    esfLongStr2="etaMax24_asymHLT_1D";
-    label1="DYee |#eta|<#color[3]{2.5}";
-    label2="DYee |#eta|<2.4 asym.HLT";
-    //fnameTag="-cmpPkg--";
-  }
-  */
-
 
   if (0) {
     path1="/home/andriusj/cms/CMSSW_3_8_4/src/DYee8TeV-20130801/DrellYanDMDY/root_files/constants/DY_j22_19789pb/";
@@ -252,7 +233,7 @@ void compareESF(int iBr=0,
   if (0) { // added 2014.02.26
     path1="../Covariance/";
     path2="../Covariance/";
-    fnameBase="covRhoFileSF";
+    fnameBase1="covRhoFileSF"; fnameBase2=fnameBase1;
     esfLongStr1="_nMB41_egamma_asymHLT_Unregressed_energy-allSyst_100";
     esfLongStr2="_nMB41_egamma_asymHLT_Unregressed_energy-noSyst_100";
     label1="EGamma (unreg.en.+all syst.)";
@@ -268,7 +249,7 @@ void compareESF(int iBr=0,
   if (0) { // added 2014.03.01
     path1="../Covariance/";
     path2="../Covariance/";
-    fnameBase="covRhoFileSF";
+    fnameBase1="covRhoFileSF"; fnameBase2=fnameBase1;
     esfLongStr1="_nMB41_egamma_asymHLT_Unregressed_energy-allSyst_100";
     esfLongStr2="_nMB41_egamma_asymHLT_Unregressed_energy-allSyst-egammaSystOnly_100";
     label1="EGamma (unreg.en.+all syst.)";
@@ -285,7 +266,7 @@ void compareESF(int iBr=0,
   if (0) { // added 2014.03.01
     path1="../Covariance/";
     path2="../Covariance/";
-    fnameBase="covRhoFileSF";
+    fnameBase1="covRhoFileSF"; fnameBase2=fnameBase1;
     esfLongStr1="_nMB41_egamma_asymHLT_Unregressed_energy-allSyst_100";
     esfLongStr2="_nMB41_egamma_asymHLT_Unregressed_energy-allSyst_1000";
     label1="EGamma (unreg.en.+all syst. 100)";
@@ -299,10 +280,10 @@ void compareESF(int iBr=0,
     setYRanges2D=enforceYRanges(1);
   }
 
-  if (1) { // added 2014.03.03
+  if (0) { // added 2014.03.03
     path1="../Covariance/";
     path2="../Covariance/";
-    fnameBase="covRhoFileSF";
+    fnameBase1="covRhoFileSF"; fnameBase2=fnameBase1;
     esfLongStr1="_nMB41_egamma_asymHLT_Unregressed_energy-recoSyst_100";
     esfLongStr2="_nMB41_egamma_asymHLT_Unregressed_energy-recoSyst-egammaSystOnly_100";
     label1="EGamma (unreg.en.+reco syst.)";
@@ -319,7 +300,7 @@ void compareESF(int iBr=0,
   if (0) { // added 2014.02.27
     path1="../Covariance/";
     path2="../Covariance/";
-    fnameBase="covRhoFileSF";
+    fnameBase1="covRhoFileSF"; fnameBase2=fnameBase1;
     esfLongStr1="_nMB41_egamma_asymHLT_regEn-allSyst_100";
     esfLongStr2="_nMB41_egamma_asymHLT_regEn_100";
     label1="EGamma (reg.en.+all syst.)";
@@ -334,7 +315,7 @@ void compareESF(int iBr=0,
   if (0) { // added 2014.02.28
     path1="../Covariance/";
     path2="../Covariance/";
-    fnameBase="covRhoFileSF";
+    fnameBase1="covRhoFileSF"; fnameBase2=fnameBase1;
     esfLongStr1="_nMB41_egamma_asymHLT_regEn-allSyst_100";
     esfLongStr2="_nMB41_egamma_asymHLT_Unregressed_energy-allSyst_100";
     label1="EGamma (reg.en.+all syst.)";
@@ -347,12 +328,42 @@ void compareESF(int iBr=0,
     swapColors=1;
   }
 
+  if (1) { // added 2014.03.08
+    path1="../Covariance/";
+    path2="../Covariance/";
+    path3="../Covariance/";
+    path4="../Covariance/";
+    fnameBase1="covRhoFileSF";
+    fnameBase2="covRhoFileSF_RECO"; 
+    fnameBase3="covRhoFileSF_ID";
+    fnameBase4="covRhoFileSF_dtHLT";
+    esfLongStr1="_nMB41_egamma_asymHLT_Unregressed_energy-allSyst_100";
+    esfLongStr2="_nMB41_egamma_asymHLT_Unregressed_energy-recoSyst_100";
+    esfLongStr3="_nMB41_egamma_asymHLT_Unregressed_energy-idSyst_100";
+    esfLongStr4="_nMB41_egamma_asymHLT_Unregressed_energy-hltSyst_100";
+    label1="full SF (unreg.en.+all syst.)";
+    label2="RECO SF (unreg.en.+RECO syst.)";
+    label3="ID SF   (unreg.en.+ID syst)";
+    label4="HLT SF  (unreg.en.+HLT syst)";
+    fnameTag="-egamma-unregEn-diffSF";
+    saveDirTag="-egamma-unregEn--";
+    if (nDim==2) { set_ratio_y_min=0.9; set_ratio_y_max=1.15; }
+    else { set_ratio_y_min=0.95; set_ratio_y_max=1.15; }
+    compSet=-10;
+    swapColors=0;
+    //setYRanges2D=enforceYRanges(1);
+  }
+
 
   if (DYTools::study2D) {
     esfLongStr1.ReplaceAll("1D","2D");
     esfLongStr2.ReplaceAll("1D","2D");
+    esfLongStr3.ReplaceAll("1D","2D");
+    esfLongStr4.ReplaceAll("1D","2D");
     esfLongStr1.ReplaceAll("nMB41","nMB7");
     esfLongStr2.ReplaceAll("nMB41","nMB7");
+    esfLongStr3.ReplaceAll("nMB41","nMB7");
+    esfLongStr4.ReplaceAll("nMB41","nMB7");
   }
 
   if (iBr!=0) {
@@ -401,12 +412,23 @@ void compareESF(int iBr=0,
     }
   }
 
-  TString fname1=path1 + fnameBase + esfLongStr1 + TString(".root");
-  TString fname2=path2 + fnameBase + esfLongStr2 + TString(".root");
+  TString fname1=path1 + fnameBase1 + esfLongStr1 + TString(".root");
+  TString fname2=path2 + fnameBase2 + esfLongStr2 + TString(".root");
 
   TH2D *h2esf1= loadESF(fname1,label1);
   h2esf1->Print("range");
   TH2D *h2esf2= (!secondIs7TeV) ? loadESF(fname2,label2) : loadESF_2D_7TeV(fname2,label2);
+
+  TH2D *h2esf3=NULL, *h2esf4=NULL;
+
+  if (fnameBase3.Length()) {
+    TString fname3= path3 + fnameBase3 + esfLongStr3 + TString(".root");
+    h2esf3= loadESF(fname3,label3);
+  }
+  if (fnameBase4.Length()) {
+    TString fname4= path4 + fnameBase4 + esfLongStr4 + TString(".root");
+    h2esf4= loadESF(fname4,label4);
+  }
 
   TH2D *h2diff= (TH2D*)h2esf1->Clone("diff");
   if (!secondIs7TeV) {
@@ -441,25 +463,40 @@ void compareESF(int iBr=0,
   if (DYTools::study2D) {
     h2esf1->Print("range");
     h2esf2->Print("range");
+    if (h2esf3) h2esf3->Print("range");
+    if (h2esf4) h2esf4->Print("range");
 
     std::vector<TH1D*> hProfEsf1,hProfEsf2;
+    std::vector<TH1D*> hProfEsf3,hProfEsf4;
+
     for (int mbin=2; mbin<=7; mbin++) {
-      TString hname1=Form("hProf1_m%d",mbin);
-      TString hname2=Form("hProf2_m%d",mbin);
-      int set_nYBins=DYTools::nYBins[mbin-1]; //(mbin==7) ? 12:24;
-      TH1D *h1= createProfileY(h2esf1,mbin,hname1,1,hname1, set_nYBins,0.,DYTools::yRangeMax+1e-4);
-      TH1D *h2= createProfileY(h2esf2,mbin,hname2,1,hname2, set_nYBins,0.,DYTools::yRangeMax+1e-4);
+      for (int iset=0; iset<4; ++iset) {
+	TH2D *AllEsf=NULL;
+	std::vector<TH1D*> *profV;
+	switch(iset) {
+	case 0: AllEsf=h2esf1; profV=&hProfEsf1; break;
+	case 1: AllEsf=h2esf2; profV=&hProfEsf2; break;
+	case 2: AllEsf=h2esf3; profV=&hProfEsf3; break;
+	case 3: AllEsf=h2esf4; profV=&hProfEsf4; break;
+	}
+	if (AllEsf==NULL) continue;
 
-      h1->GetYaxis()->SetTitleSize(0.07);
-      h1->GetYaxis()->SetTitleOffset(1.08);
-      h1->GetYaxis()->SetLabelSize(0.06);
+	TString hname1=Form("hProf%d_m%d",iset+1,mbin);
+	int set_nYBins=DYTools::nYBins[mbin-1]; //(mbin==7) ? 12:24;
+	TH1D *h1= createProfileY(AllEsf,mbin,hname1,1,hname1, set_nYBins,0.,DYTools::yRangeMax+1e-4);
 
-      hProfEsf1.push_back(h1);
-      hProfEsf2.push_back(h2);
+	h1->GetYaxis()->SetTitleSize(0.07);
+	h1->GetYaxis()->SetTitleOffset(1.08);
+	h1->GetYaxis()->SetLabelSize(0.06);
+	//if (iset>1) h1->SetMarkerStyle(24);
+	if (h2esf4) h1->SetMarkerSize(0.7);
+	
+	profV->push_back(h1);
+      }
     }
 
     std::vector<ComparisonPlot_t*> cpV;
-    for (int i=0; i<6; ++i) {
+    for (unsigned int i=0; i<6; ++i) {
       TString mRange=Form("M_%2.0lf_%2.0lf",DYTools::massBinLimits[i+1],DYTools::massBinLimits[i+2]);
       ComparisonPlot_t *cp=NULL;
       //cp=new ComparisonPlot_t(ComparisonPlot_t::_ratioRel,Form("cp_%s",mRange.Data()),mRange,"|y|","event scale factor","rel.ratio");
@@ -475,14 +512,20 @@ void compareESF(int iBr=0,
       //cp->SetRatioYRange(0.97,1.01);
       cp->SetRatioYRange(set_ratio_y_min,set_ratio_y_max);
       //cp->SetRatioYRange(-0.01,0.01);
-      cp->AddHist1D(hProfEsf1[i],label1,"LPE1",kBlack,1,0);
-      cp->AddHist1D(hProfEsf2[i],label2,"PE3",kBlue ,2,0);
+
+      int showLegend=1;
+      if ((compSet==-10) && (i!=4)) showLegend=-1;
+
+      cp->AddHist1D(hProfEsf1[i],label1,"LPE1",kBlack,1,0,showLegend);
+      cp->AddHist1D(hProfEsf2[i],label2,"PE3",kBlue ,2,0,showLegend);
+      if (i<hProfEsf3.size()) cp->AddHist1D(hProfEsf3[i],label3,"PE3",kRed+1, 3,0,showLegend);
+      if (i<hProfEsf4.size()) cp->AddHist1D(hProfEsf4[i],label4,"PE3",kGreen+2, 3,0,showLegend);
       //cp->AddHist1D(hProfEsf2[i],label2,"LPE3",kBlue ,2,0);
       cp->AddLine(0.,1.0, 2.4,1.0, kBlue+1,kDashed);
       cpV.push_back(cp);
     }
 
-    cy=new TCanvas("cy","cy",1200,800);
+    cy=new TCanvas("cy","cy",1100,800);
     cpV[0]->Prepare6Pads(cy, 1);
 
     int printRatio=0;
@@ -503,6 +546,8 @@ void compareESF(int iBr=0,
   else {
     TH1D *h1= createProfileX(h2esf1,1,"hProf1",1,"hProf1");
     TH1D *h2= createProfileX(h2esf2,1,"hProf2",1,"hProf2");
+    TH1D *h3= (h2esf3) ? createProfileX(h2esf3,1,"hProf3",1,"hProf3") : NULL;
+    TH1D *h4= (h2esf4) ? createProfileX(h2esf4,1,"hProf4",1,"hProf4") : NULL;
 
     //std::cout << "\n\ncreated profile from " << h2esf2->GetName() << "\n";
     //h2esf2->Print("range");
@@ -518,6 +563,7 @@ void compareESF(int iBr=0,
 
     int color1=kBlack;
     int color2=46; // red-brown
+    if (h3) color2=kBlue;
     if (swapColors) { int icol=color1; color1=color2; color2=icol; }
     if (swapColors==2) color1=kRed;
 
@@ -527,7 +573,10 @@ void compareESF(int iBr=0,
     cp->SetLogx();
     //h1->GetYaxis()->SetTitleOffset(1.47);
     cp->AddHist1D(h1, label1,"LPE1",color1,1,0);
-    cp->AddHist1D(h2, label2,"LPE1",color2,1,0);
+    TString opt2=(h3) ? "LPE3" : "LPE1";
+    cp->AddHist1D(h2, label2,opt2,color2,1,0);
+    if (h3) cp->AddHist1D(h3, label3, "LPE3",kRed+1,1,0);
+    if (h4) cp->AddHist1D(h4, label4, "LPE3",kGreen+2,1,0);
 
     cp->AddLine(DYTools::massBinLimits[0],1.,DYTools::massBinLimits[DYTools::nMassBins],1., kBlack,kDashed);
 
