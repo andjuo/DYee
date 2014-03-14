2748d168dbd9524e55e9bad88b964f45920b000c
Andrius Juodagalvis <andjuo@hotmail.com>
2014-03-14 14:47:07 +0200

    EventScaleFactors/calcEventEffLink.*: updated header for initManagers(). Added 3 own functions. getAsymGraph can be used to call getAsymGraph_vsEt, getAsymGraph_vsEta

--- EventScaleFactors/calcEventEffLink.cc
+++ EventScaleFactors/calcEventEffLink.cc
@@ -2,7 +2,104 @@
 
 #include "calcEventEff.C"
 
+// ------------------------------------------------------------
+
 int fillEfficiencyConstants( const InputFileMgr_t &inpMgr ) {
   DYTools::TSystematicsStudy_t systMode=DYTools::NO_SYST;
   return fillEfficiencyConstants(inpMgr,systMode);
 }
+
+// ------------------------------------------------------------
+
+TGraphAsymmErrors* getAsymGraph_vsEt(DYTools::TEtBinSet_t etBinning_inp,
+				     DYTools::TEtaBinSet_t etaBinning_inp,
+				     int iEta,
+				     const TMatrixD &Meff,
+				     const TMatrixD &MeffLo,
+				     const TMatrixD &MeffHi,
+				     TH1D  **histo,
+				     const char *histoName) {
+  int loc_etBinCount=DYTools::getNEtBins(etBinning_inp);
+  double *loc_etBinLimits=DYTools::getEtBinLimits(etBinning_inp);
+  //int loc_etaBinCount=DYTools::getNEtaBins(etaBinning_inp);
+  double *loc_etaBinLimits=DYTools::getEtaBinLimits(etaBinning_inp);
+
+  double x[loc_etBinCount], dx[loc_etBinCount];
+  double eff[loc_etBinCount], effLo[loc_etBinCount], effHi[loc_etBinCount];
+  for (int i=0; i<loc_etBinCount; ++i) {
+    x[i] = 0.5*(loc_etBinLimits[i  ] + loc_etBinLimits[i+1]);
+    dx[i]= 0.5*(loc_etBinLimits[i+1] - loc_etBinLimits[i  ]);
+    eff[i] = Meff[i][iEta];
+    effLo[i] = MeffLo[i][iEta];
+    effHi[i] = MeffHi[i][iEta];
+  }
+
+  if ((histo!=NULL) && (histoName!=NULL)) {
+    TH1D *h=new TH1D(histoName,histoName,loc_etBinCount,loc_etBinLimits);
+    h->SetDirectory(0);
+    h->SetStats(0);
+    h->GetXaxis()->SetTitle("E_{T}");
+    h->GetYaxis()->SetTitle("efficiency");
+    for (int i=0; i<loc_etBinCount; ++i) {
+      h->SetBinContent(i+1, eff[i]);
+      h->SetBinError  (i+1, 0.5*(effLo[i]+effHi[i]));
+    }
+    *histo=h;
+  }
+  
+  int signedEta=DYTools::signedEtaBinning(etaBinning_inp);
+  TString etaStr=Form("%s_%5.3lf_%5.3lf",(signedEta)?"#eta":"abs(#eta)",loc_etaBinLimits[iEta],loc_etaBinLimits[iEta+1]);
+  TGraphAsymmErrors *gr=new TGraphAsymmErrors(loc_etBinCount,x,eff,dx,dx,effLo,effHi);
+  gr->SetTitle(etaStr);
+  return gr;
+}
+
+// ------------------------------------------------------------
+
+TGraphAsymmErrors* getAsymGraph_vsEta(DYTools::TEtBinSet_t etBinning_inp,
+				      DYTools::TEtaBinSet_t etaBinning_inp,
+				      int iEt,
+				      const TMatrixD &Meff,
+				      const TMatrixD &MeffLo,
+				      const TMatrixD &MeffHi,
+				      TH1D  **histo,
+				      const char *histoName) {
+  //int loc_etBinCount=DYTools::getNEtBins(etBinning_inp);
+  double *loc_etBinLimits=DYTools::getEtBinLimits(etBinning_inp);
+  int loc_etaBinCount=DYTools::getNEtaBins(etaBinning_inp);
+  double *loc_etaBinLimits=DYTools::getEtaBinLimits(etaBinning_inp);
+
+  double x[loc_etaBinCount], dx[loc_etaBinCount];
+  double eff[loc_etaBinCount], effLo[loc_etaBinCount], effHi[loc_etaBinCount];
+  for (int i=0; i<loc_etaBinCount; ++i) {
+    x[i] = 0.5*(loc_etaBinLimits[i  ] + loc_etaBinLimits[i+1]);
+    dx[i]= 0.5*(loc_etaBinLimits[i+1] - loc_etaBinLimits[i  ]);
+    eff[i]   = Meff[iEt][i];
+    effLo[i] = MeffLo[iEt][i];
+    effHi[i] = MeffHi[iEt][i];
+  }
+
+  int signedEta=DYTools::signedEtaBinning(etaBinning_inp);
+  
+  if ((histo!=NULL) && (histoName!=NULL)) {
+    TH1D *h=new TH1D(histoName,histoName,loc_etaBinCount,loc_etaBinLimits);
+    h->SetDirectory(0);
+    h->SetStats(0);
+    TString xaxisTitle= (signedEta) ? "|#eta|" : "#eta";
+    h->GetXaxis()->SetTitle(xaxisTitle);
+    h->GetYaxis()->SetTitle("efficiency");
+    for (int i=0; i<loc_etaBinCount; ++i) {
+      h->SetBinContent(i+1, eff[i]);
+      h->SetBinError  (i+1, 0.5*(effLo[i]+effHi[i]));
+    }
+    *histo=h;
+  }
+  
+  TString titleStr=Form("Et_%1.0lf_%1.0lf",loc_etBinLimits[iEt],loc_etBinLimits[iEt+1]);
+  TGraphAsymmErrors *gr=new TGraphAsymmErrors(loc_etaBinCount,x,eff,dx,dx,effLo,effHi);
+  gr->SetTitle(titleStr);
+  return gr;
+}
+
+
+// ------------------------------------------------------------
--- EventScaleFactors/calcEventEffLink.h
+++ EventScaleFactors/calcEventEffLink.h
@@ -5,6 +5,7 @@
 #include <TString.h>
 #include <TMatrixD.h>
 #include <TGraphErrors.h>
+#include <TGraphAsymmErrors.h>
 #include "../Include/DYTools.hh"
 #include "../Include/TriggerSelection.hh"
 #include "../Include/InputFileMgr.hh"
@@ -30,7 +31,8 @@ extern double *etaBinLimits;
 
 int initManagers(const TString &confFileName, DYTools::TRunMode_t runMode,
 		 InputFileMgr_t &inpMgr, EventSelector_t &evtSelector,
-		 TString &puStr, int createDestinationDir);
+		 TString &puStr, int createDestinationDir,
+		 DYTools::TSystematicsStudy_t systMode);
 
 double getHLTefficiency(DYTools::TDataKind_t dataKind,
 			int etBin1, int etaBin1, double et1,
@@ -51,7 +53,43 @@ void drawEfficiencies(TFile *fRootOutput,
 		      std::vector<TGraphErrors*> *grMCVec=NULL);
 
 
+//
+// Original functions
+//
+
+TGraphAsymmErrors* getAsymGraph_vsEt(DYTools::TEtBinSet_t etBinning_inp,
+				     DYTools::TEtaBinSet_t etaBinning_inp,
+				     int iEta,
+				     const TMatrixD &Meff,
+				     const TMatrixD &MeffLo,
+				     const TMatrixD &MeffHi,
+				     TH1D  **histo=NULL,
+				     const char *histoName=NULL);
 
 
+
+TGraphAsymmErrors* getAsymGraph_vsEta(DYTools::TEtBinSet_t etBinning_inp,
+				      DYTools::TEtaBinSet_t etaBinning_inp,
+				      int iEt,
+				      const TMatrixD &Meff,
+				      const TMatrixD &MeffLo,
+				      const TMatrixD &MeffHi,
+				      TH1D  **histo=NULL,
+				      const char *histoName=NULL);
+
+inline
+TGraphAsymmErrors* getAsymGraph(int vsEt,
+				DYTools::TEtBinSet_t etBinning_inp,
+				DYTools::TEtaBinSet_t etaBinning_inp,
+				int iBin,
+				const TMatrixD &Meff,
+				const TMatrixD &MeffLo,
+				const TMatrixD &MeffHi,
+				TH1D  **histo=NULL,
+				const char *histoName=NULL) {
+  return (vsEt) ? 
+    getAsymGraph_vsEt(etBinning_inp,etaBinning_inp,iBin,Meff,MeffLo,MeffHi,histo,histoName) :
+    getAsymGraph_vsEta(etBinning_inp,etaBinning_inp,iBin,Meff,MeffLo,MeffHi,histo,histoName);
+}
 
 #endif
