516951aeb663b6dcce79db3e89bb9ef5ddf00c66
Andrius Juodagalvis <andjuo@hotmail.com>
2014-03-20 14:03:58 +0200

    YieldsAndBackgrounds/subtractBackground.C Minor fix: ensured that the systematic error histo has no value given

--- YieldsAndBackgrounds/subtractBackground.C
+++ YieldsAndBackgrounds/subtractBackground.C
@@ -74,6 +74,12 @@ int subtractBackground(const TString conf,
   //std::cout <<" " << inpMgr.yieldFullName(-1,systMode,0) << "\n";
   //std::cout <<" " << inpMgr.signalYieldFullName(systMode) << "\n";
 
+  // Save background-subtracted signal yields
+  // inputDir+TString("/yields_bg-subtracted.root")
+  const int ignoreDebugRunFlag=0;
+  TString outFileName= inpMgr.signalYieldFullFileName(systMode,ignoreDebugRunFlag);
+  std::cout << "generated outFileName=<" << outFileName << ">\n";
+
   //--------------------------------------------------------------------------------------------------------------
   // Main analysis code 
   //============================================================================================================== 
@@ -205,6 +211,7 @@ int subtractBackground(const TString conf,
       else {
 	// Here it is added in quadrature
 	TH2D *weights=(TH2D*)yields[i]->Clone(yields[i]->GetName() + TString("_clone"));
+	removeError(weights);
 	swapContentAndError(weights);
 	mcbkgTrue2e.addSystErr(weights,1.);
 	delete weights;
@@ -223,9 +230,11 @@ int subtractBackground(const TString conf,
   }
 
   if (add_WZZZ_error_old_way) {
-    swapContentAndError(hWZZZ);
-    mcbkgTrue2e.addSystErr(hWZZZ,1.);
-    swapContentAndError(hWZZZ);
+    TH2D *weights=(TH2D*)hWZZZ->Clone(hWZZZ->GetName() + TString("_clone"));
+    removeError(weights);
+    swapContentAndError(weights);
+    mcbkgTrue2e.addSystErr(weights,1.);
+    delete weights;
   }
 
   if (0) {
@@ -311,10 +320,8 @@ int subtractBackground(const TString conf,
   signalYieldDDbkg.print();
   zeeMCShapeReweight_mcBkg->Print("range");
 
-  // Save background-subtracted signal yields
-  // inputDir+TString("/yields_bg-subtracted.root")
-  const int ignoreDebugRunFlag=0;
-  TString outFileName= inpMgr.signalYieldFullFileName(systMode,ignoreDebugRunFlag);
+  std::cout << "outFileName=<" << outFileName << ">\n";
+
   TFile fileOut(outFileName,"recreate");
   // main results 
   signalYieldMCbkg.Write();
