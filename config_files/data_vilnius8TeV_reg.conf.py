61af5a76012b967700db6c58784dde078f452c1d
Andrius Juodagalvis <andjuo@hotmail.com>
2014-03-20 17:57:33 +0200

    config_files/data_vilnius8TeV_reg.conf.py updated DDBkgVersion

--- config_files/data_vilnius8TeV_reg.conf.py
+++ config_files/data_vilnius8TeV_reg.conf.py
@@ -67,7 +67,8 @@ seedMin=1001
 seedMax=1020
 Use7TeVMCWeight=1 # Whether renormalize the 1st sample weight to 1 in MC macros
 IgnoreDebugRunForYields=1 # prepareYields shouldn't use DebugRun ntuples
-DDBkgVersion=20131231
+#DDBkgVersion=20131231
+DDBkgVersion=20140312  # from Manny through Alexey
 
 [TAG_AND_PROBE]
 # Tag and probe options
