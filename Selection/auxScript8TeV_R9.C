a45ed2ef56ceb41e5ce6376ae6c2ef605d2de0c2
Andrius Juodagalvis <andjuo@hotmail.com>
2014-03-23 21:41:22 +0200

    Selection/auxScript8TeV_R9.C added LOWER_ET_CUT systMode

--- Selection/auxScript8TeV_R9.C
+++ Selection/auxScript8TeV_R9.C
@@ -3,5 +3,6 @@
   //selectEvents("../config_files/data_vilnius7TeV.conf.py",DYTools::DEBUG_RUN,DYTools::NO_SYST);
   //selectEvents("../config_files/data_vilnius7TeV.conf.py",DYTools::NORMAL,DYTools::NO_SYST);
   //selectEventsR9("../config_files/data_vilnius8TeV_regSSD.conf.py",DYTools::DEBUG_RUN,DYTools::NO_SYST);
-  selectEventsR9("../config_files/data_vilnius8TeV_regSSD.conf.py",DYTools::NORMAL_RUN,DYTools::NO_SYST);
+  //selectEventsR9("../config_files/data_vilnius8TeV_regSSD.conf.py",DYTools::NORMAL_RUN,DYTools::NO_SYST);
+  selectEventsR9("../config_files/data_vilnius8TeV_regSSD.conf.py",DYTools::NORMAL_RUN,DYTools::LOWER_ET_CUT);
 }
