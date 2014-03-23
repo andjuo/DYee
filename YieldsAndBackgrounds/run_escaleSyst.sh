8310213898118e45dbb9ba44c67d15f02d07ab63
Andrius Juodagalvis <andjuo@hotmail.com>
2014-03-23 21:55:41 +0200

    YieldsAndBackgrounds/run_escaleSyst.sh: added calc_escaleDiffSyst.C

--- YieldsAndBackgrounds/run_escaleSyst.sh
+++ YieldsAndBackgrounds/run_escaleSyst.sh
@@ -4,13 +4,13 @@ inpFile="../config_files/data_vilnius8TeV_regSSD.conf.py"
 
 runPrepYields=1
 runSubtractBkg=1
-runPlotSyst=0
+runCalcSyst=1
 
 runMode=DYTools::NORMAL_RUN
 
 systList="DYTools::ESCALE_DIFF_0000 DYTools::ESCALE_DIFF_0005 DYTools::ESCALE_DIFF_0010 DYTools::ESCALE_DIFF_0015 DYTools::ESCALE_DIFF_0020"
 
-systList="DYTools::ESCALE_DIFF_0005 DYTools::ESCALE_DIFF_0010 DYTools::ESCALE_DIFF_0015 DYTools::ESCALE_DIFF_0020"
+#systList="DYTools::ESCALE_DIFF_0005 DYTools::ESCALE_DIFF_0010 DYTools::ESCALE_DIFF_0015 DYTools::ESCALE_DIFF_0020"
 
 #systList="DYTools::ESCALE_DIFF_0000"
 timeStamp="-`date +%Y%m%d-%H%M`"
@@ -31,3 +31,7 @@ for systMode in ${systList} ; do
 	| tee log-subtractBackgroundR9-${runModeStr}-${systModeStr}-${timeStamp}.log
   fi
 done
+
+if [ ${runCalcSyst} -eq 1 ] ; then
+    root -l -q -b calc_escaleDiffSyst.C+\(0,\"${inpFile}\",DYTools::NORMAL_RUN\) | tee log-calc_escaleDiffSyst-${runModeStr}-${systModeStr}-${timeStamp}.log
+fi
