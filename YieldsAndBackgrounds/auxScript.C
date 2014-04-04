9a1860097c19f95d2c07e723f75afbada84ce7d5
Andrius Juodagalvis <andjuo@hotmail.com>
2014-04-04 15:39:49 +0300

    YieldsAndBackgrounds/ remove auxScript.C prepareYields.C subtractBackground.C. Default versions will be 'R9'

--- YieldsAndBackgrounds/auxScript.C
+++
@@ -1,8 +0,0 @@
-{
-  gROOT->ProcessLine(".L prepareYields.C+");
-  prepareYields("../config_files/data_vilnius8TeV_regSSD.conf.py",DYTools::NORMAL_RUN,DYTools::NO_SYST);
-  //prepareYields("../config_files/data_vilnius8TeV_reg.conf.py",DYTools::DEBUG_RUN,DYTools::NO_SYST);
-
-  gROOT->ProcessLine(".L subtractBackground.C+");
-  subtractBackground("../config_files/data_vilnius8TeV_regSSD.conf.py",DYTools::NORMAL_RUN,DYTools::NO_SYST);
-}
