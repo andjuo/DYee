6f88fef9e24574f974b8b965038b853f22f56932
Andrius Juodagalvis <andjuo@hotmail.com>
2014-04-04 18:36:41 +0300

    Efficiency/plot_effSystematics.C update to current setup

--- Efficiency/plot_effSystematics.C
+++ Efficiency/plot_effSystematics.C
@@ -1,5 +1,6 @@
 #include "../Efficiency/plot_Systematics.C"
 
-void plot_effSystematics(TString flags="111", int saveCanvas=0) {
-  plot_Systematics("efficiency",flags,saveCanvas);
+void plot_effSystematics(int analysisIs2D,
+			 TString flags="111", int saveCanvas=0) {
+  plot_Systematics(analysisIs2D,"efficiency",flags,saveCanvas);
 }
