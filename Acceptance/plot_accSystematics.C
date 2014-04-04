3793168a351fe0342c1798bc3e8eef05bfff9563
Andrius Juodagalvis <andjuo@hotmail.com>
2014-04-04 18:32:21 +0300

    Acceptance/plot_accSystematics.C update to current setup

--- Acceptance/plot_accSystematics.C
+++ Acceptance/plot_accSystematics.C
@@ -1,5 +1,6 @@
 #include "../Efficiency/plot_Systematics.C"
 
-void plot_accSystematics(TString flags="110", int saveCanvas=0) {
-  plot_Systematics("acceptance",flags,saveCanvas);
+void plot_accSystematics(int analysisIs2D,
+			 TString flags="101", int saveCanvas=0) {
+  plot_Systematics(analysisIs2D,"acceptance",flags,saveCanvas);
 }
