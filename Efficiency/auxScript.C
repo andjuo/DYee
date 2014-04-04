589df1885e2b7dfac74b32975348cd817d3f9678
Andrius Juodagalvis <andjuo@hotmail.com>
2014-04-04 18:37:59 +0300

    Efficiency/auxScript.C update to current setup

--- Efficiency/auxScript.C
+++ Efficiency/auxScript.C
@@ -1,7 +1,7 @@
 {
   gROOT->ProcessLine(".L plotDYEfficiency.C+");
-  //plotDYEfficiency("../config_files/data_vilnius8TeV.conf.py",DYTools::DEBUG_RUN,DYTools::NO_SYST);
-  plotDYEfficiency("../config_files/data_vilnius8TeV.conf.py",DYTools::NORMAL_RUN,DYTools::NO_SYST);
-  //plotDYEfficiency("../config_files/data_vilnius8TeV.conf.py",DYTools::DEBUG_RUN,DYTools::FSR_STUDY,1.05,1.);
+  //plotDYEfficiency(1,"../config_files/data_vilnius8TeV.conf.py",DYTools::DEBUG_RUN,DYTools::NO_SYST);
+  plotDYEfficiency(1,"default",DYTools::NORMAL_RUN,DYTools::NO_SYST);
+  //plotDYEfficiency(1,"../config_files/data_vilnius8TeV.conf.py",DYTools::DEBUG_RUN,DYTools::FSR_STUDY,1.05,1.);
 
 }
