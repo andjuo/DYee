96134cd7871a5691a6f6e7fa046ef8aeee9c1921
Andrius Juodagalvis <andjuo@hotmail.com>
2014-04-04 14:02:23 +0300

    Acceptance/auxScript.C adaptation to latest changes (dim flag, default inp file)

--- Acceptance/auxScript.C
+++ Acceptance/auxScript.C
@@ -1,7 +1,6 @@
 {
   gROOT->ProcessLine(".L plotDYAcceptance.C+");
-  //plotDYAcceptance("../config_files/data_vilnius8TeV.conf.py",DYTools::DEBUG_RUN,DYTools::NO_SYST);
-  plotDYAcceptance("../config_files/data_vilnius8TeV.conf.py",DYTools::NORMAL_RUN,DYTools::NO_SYST);
-  //plotDYAcceptance("../config_files/data_vilnius8TeV.conf.py",DYTools::DEBUG_RUN,DYTools::FSR_STUDY,1.05,1.);
-
+  //plotDYAcceptance(1,"../config_files/data_vilnius8TeV.conf.py",DYTools::DEBUG_RUN,DYTools::NO_SYST);
+  plotDYAcceptance(1,"default",DYTools::NORMAL_RUN,DYTools::NO_SYST);
+  //plotDYAcceptance(1,"default",DYTools::DEBUG_RUN,DYTools::FSR_STUDY,1.05,1.);
 }
