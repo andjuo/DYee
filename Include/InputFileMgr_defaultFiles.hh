90031e95dd16264b724a413df47e856325a1d3e9
Andrius Juodagalvis <andjuo@hotmail.com>
2014-04-05 14:35:12 +0300

    InputFileMgr_defaultFiles.hh : added more default files

--- Include/InputFileMgr_defaultFiles.hh
+++ Include/InputFileMgr_defaultFiles.hh
@@ -3,8 +3,17 @@
 
 TString getDefaultFileName(TString index) {
   TString fname;
-  if (index=="default") {
+  if (index=="defaultRemote") {
+    fname="../config_files/data_vilnius8TeVremote_regSSD.conf.py";
+  }
+  else if (index=="default") {
     fname="../config_files/data_vilnius8TeV_regSSD.conf.py";
+  }
+  if (index=="defaultAdHocRemote") {
+    fname="../config_files/data_vilnius8TeVremote_regSSD_adHoc.conf.py";
+  }
+  else if (index=="defaultAdHoc") {
+    fname="../config_files/data_vilnius8TeV_regSSD_adHoc.conf.py";
   }
   else if (index=="defaultOld") {
     fname="../config_files/old-data_vilnius8TeV_regSSD.conf.py";
