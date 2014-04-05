71dfc3725e1ffad63dbeb7098cb51fab20c3b6ef
Andrius Juodagalvis <andjuo@hotmail.com>
2014-04-05 16:21:56 +0300

    Unfolding/auxScript.C update to current setup

--- Unfolding/auxScript.C
+++ Unfolding/auxScript.C
@@ -1,7 +1,7 @@
 {
   gROOT->ProcessLine(".L plotUnfoldingMatrix.C+");
-  //plotUnfoldingMatrix("../config_files/data_vilnius8TeV.conf.py",DYTools::DEBUG_RUN,DYTools::NO_SYST);
-  plotUnfoldingMatrix("../config_files/data_vilnius8TeV.conf.py",DYTools::NORMAL_RUN,DYTools::NO_SYST);
-  //plotUnfoldingMatrix("../config_files/data_vilnius8TeV.conf.py",DYTools::DEBUG_RUN,DYTools::FSR_STUDY,1.05,1.);
+  //plotUnfoldingMatrix(1,"../config_files/data_vilnius8TeV.conf.py",DYTools::DEBUG_RUN,DYTools::NO_SYST);
+  //plotUnfoldingMatrix(1,"default",DYTools::NORMAL_RUN,DYTools::NO_SYST);
+  plotUnfoldingMatrix(1,"default",DYTools::DEBUG_RUN,DYTools::FSR_STUDY);
 
 }
