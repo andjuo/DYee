6d123c34ad71bced1a5a97bff586cc7bb97d154c
Andrius Juodagalvis <andjuo@hotmail.com>
2014-04-08 18:13:20 +0300

    YieldsAndBackgrounds/auxScript_subBkg.C updated to current setup

--- YieldsAndBackgrounds/auxScript_subBkg.C
+++ YieldsAndBackgrounds/auxScript_subBkg.C
@@ -2,12 +2,13 @@
 #include "../Include/DYTools.hh"
 #include "subtractBackground.C"
 
-int auxScript_subBkg() {
+int auxScript_subBkg(int analysisIs2D) {
   //gROOT->ProcessLine(".L prepareYields.C+");
   //prepareYields("../config_files/data_vilnius8TeV_regSSD.conf.py",DYTools::NORMAL_RUN,DYTools::NO_SYST);
   //prepareYields("../config_files/data_vilnius8TeV_reg.conf.py",DYTools::DEBUG_RUN,DYTools::NO_SYST);
 
   //gROOT->ProcessLine(".L subtractBackground.C+");
-  int res=subtractBackground("../config_files/data_vilnius8TeV_regSSD.conf.py",DYTools::NORMAL_RUN,DYTools::NO_SYST);
+  //int res=subtractBackground(analysisIs2D,"default",DYTools::NORMAL_RUN,DYTools::NO_SYST);
+  int res=subtractBackground(analysisIs2D,"default",DYTools::NORMAL_RUN,DYTools::ESCALE_DIFF_0000);
   return res;
 }
