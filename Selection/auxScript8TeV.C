2b677c59a1c492bf70a12d270115912b132c651e
Andrius Juodagalvis <andjuo@hotmail.com>
2014-04-08 18:12:00 +0300

    Selection/auxScript8TeV.C changed the default running

--- Selection/auxScript8TeV.C
+++ Selection/auxScript8TeV.C
@@ -1,7 +1,7 @@
 {
   gROOT->ProcessLine(".L selectEvents.C+");
   // The first number tells if the analysis is 2D
-  selectEvents(1,"default",DYTools::DEBUG_RUN,DYTools::NO_SYST);
+  //selectEvents(1,"default",DYTools::DEBUG_RUN,DYTools::NO_SYST);
   //selectEvents(1,"default",DYTools::NORMAL_RUN,DYTools::NO_SYST);
-  //selectEvents(1,"default",DYTools::NORMAL_RUN,DYTools::LOWER_ET_CUT);
+  selectEvents(1,"default",DYTools::NORMAL_RUN,DYTools::LOWER_ET_CUT);
 }
