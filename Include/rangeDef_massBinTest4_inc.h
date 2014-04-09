3025108eef04d2966d08d112ba5a47d932d27a1b
Andrius Juodagalvis <andjuo@hotmail.com>
2014-04-09 15:04:53 +0300

    Include/rangeDef_massBinTest4_inc.h updated to current setup

--- Include/rangeDef_massBinTest4_inc.h
+++ Include/rangeDef_massBinTest4_inc.h
@@ -1,9 +1,16 @@
 #warning included rangeDef_massBinTest4_inc.h
 
-const TString analysisTag_binning="_mbTest4";
-const DYTools::TMassBinning_t massBinningSet= _MassBins_test4;
+namespace DYTools {
+
+const TString _defined_analysisTag_binning="_mbTest4";
+const DYTools::TMassBinning_t _defined_massBinningSet= _MassBins_test4;
+
+const double _extend_Y = 6.5;
+const double _yRangeEdge= electronEtaMax;
+const double _yRangeMax = electronEtaMax;
+
 const int _nMassBins1D = 4;
-const double _massBinLimits1D[_nMassBins1D+1] = 
+const double _massBinLimits1D[_nMassBins1D+1] =
   {15,45,60,120,1500}; // 4 bins with Z-peak region singled-out
 const int _nYBinsMax1D=1; // the largest division into Y bins
 const int _nYBins1D[_nMassBins1D] = { 1, 1, 1, 1 };
@@ -13,6 +20,4 @@ const double *_massBinLimits2D= _massBinLimits1D;
 const int _nYBinsMax2D=2;
 const int _nYBins2D[_nMassBins1D] = { 2, 2, 2, 1 };
 
-
-const double _yRangeEdge=_yRangeEdge_base;
-const double _yRangeMax=_yRangeMax_base; 
+}; // namespace DYTools
