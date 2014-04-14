2406357dd56189ca732d1717d6b07f2610105d86
Andrius Juodagalvis <andjuo@hotmail.com>
2014-04-14 18:50:03 +0300

    YieldsAndBackgrounds/subtractBackground.C updated to current set-up, including rnd studies

--- YieldsAndBackgrounds/run_subtractBackground.C
+++ YieldsAndBackgrounds/run_subtractBackground.C
@@ -1,11 +1,12 @@
 #include "subtractBackground.C"
 
 int run_subtractBackground(int analysisIs2D,
-			   int the_case, int systModeFlag=0, int loadData=1) {
+			   int the_case, int systModeFlag=0, int debug=-1) {
   TString confName="default";
+  DYTools::TRunMode_t runMode=DebugInt2RunMode(debug);
 
-  DYTools::TRunMode_t runMode=(loadData) ? DYTools::LOAD_DATA : DYTools::NORMAL_RUN;
-  DYTools::TSystematicsStudy_t systMode=DYTools::NO_SYST;
+  //DYTools::TSystematicsStudy_t systMode=DYTools::NO_SYST;
+  DYTools::TSystematicsStudy_t systMode=DYTools::ESCALE_DIFF_0000;
 
   switch(systModeFlag) {
   case 0: ; break;
@@ -18,11 +19,51 @@ int run_subtractBackground(int analysisIs2D,
     confName="defaultAdHoc";
     systMode=DYTools::APPLY_ESCALE;
     break;
+  case 2:
+  case 3:
+    confName="defaultAdHoc";
+    systMode=DYTools::ESCALE_STUDY_RND;
+    break;
   default:
     std::cout << "the_case=" << the_case << " is not ready\n";
     return retCodeError;
   }
 
-  int res=subtractBackgroundR9(confName,runMode,systMode);
+  // Setup the mass-rapidity setting
+  if (!DYTools::setup(analysisIs2D)) {
+    std::cout << "failed to initialize the analysis\n";
+    return retCodeError;
+  }
+
+  analysisIs2D=-111; // further calls to DYTools::setup do nothing
+  int res=1;
+
+  if (systMode!=DYTools::ESCALE_STUDY_RND) {
+    res=subtractBackground(analysisIs2D,confName,runMode,systMode);
+  }
+  else {
+    gBenchmark->Start("run_subtractBackground");
+    int iSeedMin=-1;
+    int iSeedMax=-1;
+    int dSeed=1;
+    if (the_case!=3) {
+      InputFileMgr_t inpMgr;
+      if (!inpMgr.Load(confName)) return retCodeError;
+      iSeedMin=inpMgr.userKeyValueAsInt("SEEDMIN");
+      iSeedMax=inpMgr.userKeyValueAsInt("SEEDMAX");
+    }
+    else {
+      iSeedMin=-111;
+      iSeedMax= 111;
+      dSeed=iSeedMax-iSeedMin;
+    }
+    if (res) res=retCodeOk;
+    for (int iSeed=iSeedMin; (res==retCodeOk) && (iSeed<=iSeedMax);
+	 iSeed+=dSeed) {
+      std::cout << "\n\n\tstarting iSeed=" << iSeed << "\n\n";
+      res=subtractBackground(analysisIs2D,confName,runMode,systMode,iSeed);
+    }
+    ShowBenchmarkTime("run_subtractBackground");
+  }
   return res;
 }
