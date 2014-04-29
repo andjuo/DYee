73dfa83d2e984ef33707310dbdbff7905bd8723f
Andrius Juodagalvis <andjuo@hotmail.com>
2014-04-29 14:24:18 +0300

    EventScaleFactors/aux_syst.sh changed obsolete systModes to TAG_PT and TAG_ID

--- EventScaleFactors/aux_syst.sh
+++ EventScaleFactors/aux_syst.sh
@@ -38,10 +38,10 @@ fi
 
 
 systMode="DYTools::NO_SYST"; systName=""
-systMode="DYTools::RESOLUTION_STUDY"; systName="-tagLowerPt"
+systMode="DYTools::TAG_PT"; systName="-tagLowerPt"
 systMode="DYTools::PILEUP_5plus"; systName="-pu5plus"
 systMode="DYTools::PILEUP_5minus"; systName="-pu5minus"
-#systMode="DYTools::FSR_STUDY"; systName="-tagMediumID"
+#systMode="DYTools::TAG_ID"; systName="-tagMediumID"
 
 source evaluateESF.sh ${filename} ${debugMode} ${tnpFullRun_eval} ${systMode} | tee ${logDir}/out${timeStamp}-12-evaluateESF-efficiencyScaleFactors${anTag}-${systName}.log
 source recalcESF.sh ${analysisIs2D} ${filename} ${debugMode} ${tnpFullRun_recalc} ${systMode} | tee ${logDir}/out${timeStamp}-12-recalcESF-efficiencyScaleFactors${anTag}-${systName}.log
@@ -51,8 +51,8 @@ exit
 
 tnpFullRun_eval="data00000mc00011sf0"
 
-#systMode="DYTools::RESOLUTION_STUDY"; systName="-tagLowerPt"
-systMode="DYTools::FSR_STUDY"; systName="-tagMediumID"
+#systMode="DYTools::TAG_PT"; systName="-tagLowerPt"
+systMode="DYTools::TAG_ID"; systName="-tagMediumID"
 systMode="DYTools::PILEUP_5plus"; systName="-pu5plus"
 
 source evaluateESF.sh ${filename} ${debugMode} ${tnpFullRun_eval} ${systMode} | tee ${logDir}/out${timeStamp}-12-evaluateESF-efficiencyScaleFactors${anTag}-${systName}.log
