6aad7c9cc1d294dd255da68148fcbd1265350f1e
Andrius Juodagalvis <andjuo@hotmail.com>
2014-04-29 13:50:01 +0300

    EventScaleFactors/aux*.C update to analysisIs2D

--- EventScaleFactors/auxScript.C
+++ EventScaleFactors/auxScript.C
@@ -1,7 +1,6 @@
 {
   gROOT->ProcessLine(".L calcEventEff.C+");
-  //calcEventEff("../config_files/data_vilnius8TeV.conf.py",1,DYTools::NORMAL_RUN);
-  calcEventEff("../config_files/data_vilnius8TeV.conf.py",0,DYTools::NORMAL_RUN);
-  //calcEventEff("../config_files/data_vilnius8TeV.conf.py",0,DYTools::DEBUG_RUN);
-
+  //calcEventEff(0,"default",1,DYTools::NORMAL_RUN);
+  calcEventEff(0,"default",0,DYTools::NORMAL_RUN);
+  //calcEventEff(0,"default",0,DYTools::DEBUG_RUN);
 }
--- EventScaleFactors/auxScript_compareHLTEff.C
+++ EventScaleFactors/auxScript_compareHLTEff.C
@@ -1,7 +1,7 @@
 {
   gROOT->ProcessLine(".L compareHLTEff.C+");
-  //calcEventEff("../config_files/data_vilnius8TeV.conf.py",1,DYTools::NORMAL_RUN);
-  compareHLTEff("../config_files/data_vilnius8TeV.conf.py",DYTools::NORMAL_RUN);
-  //calcEventEff("../config_files/data_vilnius8TeV.conf.py",0,DYTools::DEBUG_RUN);
+  //calcEventEff(0,"../config_files/data_vilnius8TeV.conf.py",1,DYTools::NORMAL_RUN);
+  compareHLTEff(0,"../config_files/data_vilnius8TeV.conf.py",DYTools::NORMAL_RUN);
+  //calcEventEff(0,"../config_files/data_vilnius8TeV.conf.py",0,DYTools::DEBUG_RUN);
 
 }
