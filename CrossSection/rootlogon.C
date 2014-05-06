8dd86c2c64fb479584061864b86e2cb3e9faef27
Andrius Juodagalvis <andjuo@hotmail.com>
2014-05-06 22:01:18 +0300

    CrossSection/rootlogon.C added linking of CSCovWorkFlags.cc

--- CrossSection/rootlogon.C
+++ CrossSection/rootlogon.C
@@ -2,4 +2,6 @@
 
   gROOT->ProcessLine(".x ../Include/rootlogon.C");
   gROOT->ProcessLine(".L crossSectionFnc.cc+");
+  gROOT->ProcessLine(".L CSCovWorkFlags.cc+");
+  //gROOT->ProcessLine(".L CSCovWorkFlags.hh+");
 }
