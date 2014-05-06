8419bedf035a210ba417d819a65724247f23d9cc
Andrius Juodagalvis <andjuo@hotmail.com>
2014-05-06 10:35:02 +0300

    YieldsAndBackgrounds/plotMultiEEEvents.C intermediate modifaction to look at multi-ee events

--- YieldsAndBackgrounds/plotMultiEEEvents.C
+++ YieldsAndBackgrounds/plotMultiEEEvents.C
@@ -12,8 +12,9 @@ int plotMultiEEEvents(TString confName,
 	    DYTools::TSystematicsStudy_t systMode=DYTools::NO_SYST)
 {
 
-  if (confName="default") {
-    confName="../config_files/data_vilnius8TeV_regSSD.conf.py";
+  if (!DYTools::setup(0)) {
+    std::cout << "failed to setup the analysis\n";
+    return retCodeError;
   }
 
   if (DYTools::study2D) {
@@ -33,7 +34,7 @@ int plotMultiEEEvents(TString confName,
   //inpMgr.clearEnergyScaleTag();
 
   // Construct eventSelector, update mgr and plot directory
-  TString extraTag="R9";
+  TString extraTag="";
   TString plotExtraTag;
   EventSelector_t evtSelector(inpMgr,runMode,systMode,
 	       extraTag, plotExtraTag, EventSelector::_selectDefault);
@@ -47,25 +48,33 @@ int plotMultiEEEvents(TString confName,
   //==============================================================================================================
 
   vector<TH2D*> yields; // non-uniform rapidity grid taken into account
-  vector<TH2D*> yieldsSameEvent_BaseH2; // multi-dielectron events
-  vector<TH2D*> yieldsSameEvent; // multi-dielectron events
+  vector<TH2D*> yieldsSameEventLead_BaseH2; // multi-dielectron events
+  vector<TH2D*> yieldsSameEventTail_BaseH2; // multi-dielectron events
+  vector<TH2D*> yieldsSameEventLead; // multi-dielectron events
+  vector<TH2D*> yieldsSameEventTail; // multi-dielectron events
 
   // the main yield
   createBaseH2Vec(yields,"hYield_",inpMgr.sampleNames());
   // distribution of the same-event candidates
-  createBaseH2Vec(yieldsSameEvent_BaseH2,
-		  "hYieldsSameEvent_BaseH2_",inpMgr.sampleNames());
+  createBaseH2Vec(yieldsSameEventLead_BaseH2,
+		  "hYieldsSameEventLead_BaseH2_",inpMgr.sampleNames());
+  createBaseH2Vec(yieldsSameEventTail_BaseH2,
+		  "hYieldsSameEventTail_BaseH2_",inpMgr.sampleNames());
 
   TFile file(yieldFullName,"read");
   int res=1;
   if (res) res=checkBinningArrays(file);
   if (res) res=loadVec(file,yields,"yields");
-  if (res) res=loadVec(file,yieldsSameEvent_BaseH2,"yieldsSameEvent");
+  if (res) res=loadVec(file,yieldsSameEventLead_BaseH2,"yieldsSameEventLead");
+  if (res) res=loadVec(file,yieldsSameEventTail_BaseH2,"yieldsSameEventTail");
   file.Close();
   if (!res) return retCodeError;
 
-  if (!convertBaseH2actualVec(yieldsSameEvent_BaseH2, yieldsSameEvent,
-			      "hYieldsSameEvent_",inpMgr.sampleNames(),
+  if (!convertBaseH2actualVec(yieldsSameEventLead_BaseH2, yieldsSameEventLead,
+			      "hYieldsSameEventLead_",inpMgr.sampleNames(),
+			      1)) return retCodeError;
+  if (!convertBaseH2actualVec(yieldsSameEventTail_BaseH2, yieldsSameEventTail,
+			      "hYieldsSameEventTail_",inpMgr.sampleNames(),
 			      1)) return retCodeError;
 
 
@@ -97,17 +106,26 @@ int plotMultiEEEvents(TString confName,
       std::cout << "plot_case=" << plot_case << " is not ready\n";
       return retCodeError;
     }
-    vector<TH2D*> *histosAsH2D=NULL;
+    vector<TH2D*> *histosLeadAsH2D=NULL;
+    vector<TH2D*> *histosTailAsH2D=NULL;
     switch(plot_case) {
-    case 0: histosAsH2D=&yieldsSameEvent; ymax=600; break;
+    case 0:
+      histosLeadAsH2D=&yieldsSameEventLead;
+      histosTailAsH2D=&yieldsSameEventTail;
+      ymax=600;
+      break;
     default:
       std::cout << "plot_case=" << plot_case << " is not ready (2)\n";
       return retCodeError;
     }
     //ymax=1e6;
 
-    std::vector<std::vector<TH1D*>*> histos;
-    if (!createMassProfileVec(*histosAsH2D,histos,inpMgr.sampleNames()))
+    std::vector<std::vector<TH1D*>*> histosLead;
+    std::vector<std::vector<TH1D*>*> histosTail;
+    if (!createMassProfileVec(*histosLeadAsH2D,histosLead,inpMgr.sampleNames())
+	||
+	!createMassProfileVec(*histosTailAsH2D,histosTail,inpMgr.sampleNames())
+	)
       return retCodeError;
 
     TString cpName=TString("cp_") + plotTag;
@@ -126,22 +144,29 @@ int plotMultiEEEvents(TString confName,
     cp.SetLogy(1); cp.SetYRange(1e-3,1e3);
 
     TString hName=Form("hSumEE_%d",plot_case);
-    TH1D *h=(TH1D*)(*histos[0])[0]->Clone(hName);
+    TH1D *h=(TH1D*)(*histosLead[0])[0]->Clone(hName);
     h->Reset();
     h->SetDirectory(0);
     h->SetTitle(hName);
 
     cp.AddHist1D(hData,"data","LP skip",kWhite,1,1,-1);
-    for (unsigned int ih=0; ih<histos[0]->size(); ++ih) {
-      TH1D* currH=(*histos[0])[ih];
+    for (unsigned int ih=0; ih<histosLead[0]->size(); ++ih) {
+      TH1D* currHlead=(*histosLead[0])[ih];
+      TH1D* currHtail=(*histosTail[0])[ih];
+      TString strLead=inpMgr.sampleName(ih) + TString(" lead");
+      TString strTail=inpMgr.sampleName(ih) + TString(" tail");
       if (ih==0) {
-	cp.AddHist1D(currH,inpMgr.sampleName(ih),"LP",kBlack,1,0,1);
+	cp.AddHist1D(currHlead,strLead,"LP",kBlack,1,0,1);
+	cp.AddHist1D(currHtail,strTail,"LP",TAttMarker(kBlack,20,1.),1,0,1);
 	//printHisto(currH);
       }
       else {
-	h->Add(currH,1.);
-	int color=inpMgr.sampleInfo(ih)->colors[0];
-	cp.AddToStack(currH,inpMgr.sampleName(ih),color);
+	h->Add(currHlead,1.);
+	h->Add(currHtail,1.);
+	int colorLead=inpMgr.sampleInfo(ih)->colors[0];
+	int colorTail=kYellow+(ih%2);
+	cp.AddToStack(currHlead,strLead,colorLead);
+	cp.AddToStack(currHtail,strTail,colorTail);
       }
     }
     h->SetMarkerStyle(20);
