79eb15913c159df776a26d6965b7a57fa42fdadb
Andrius Juodagalvis <andjuo@hotmail.com>
2014-05-08 14:50:17 +0300

    EventScaleFactors/rootlogon.C changed the position of fitFunctions.cc to match current dependencies

--- EventScaleFactors/rootlogon.C
+++ EventScaleFactors/rootlogon.C
@@ -5,8 +5,8 @@
   gROOT->ProcessLine(".L RooCMSShape.cc+");
   gROOT->ProcessLine(".L cutFunctions.cc+");
   gROOT->ProcessLine(".L fitFunctionsCore.cc+");
-  gROOT->ProcessLine(".L fitFunctions.cc+");
   gROOT->ProcessLine(".L tnpSelectEvents.hh+");
+  gROOT->ProcessLine(".L fitFunctions.cc+");
 
   //
   // Some compare*C macros need calcEventEffLink.cc
