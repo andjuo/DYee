bf61ad15c41ae47a1e96734e92f22c7c28437ca9
Andrius Juodagalvis <andjuo@hotmail.com>
2014-05-29 15:19:41 +0300

    CrossSection/runBLUE_selfTest1D.C updated to check the three versions of the 1D cross section (absolute,normalized,rshape)

--- CrossSection/runBLUE_selfTest1D.C
+++ CrossSection/runBLUE_selfTest1D.C
@@ -1,10 +1,22 @@
 #include "../BLUE/resultCombiner.C"
 
-void runBLUE_selfTest1D() {
+void runBLUE_selfTest1D(int normalized=0) {
   //TString xs_fname="xsecForBlue-1D-yieldStatOnly-fromCS.dat";
   //TString cov_fname="covForBlue-1D-yieldStatOnly.dat";
-  TString xs_fname="xsecForBlue-1D-fromCov.dat";
-  TString cov_fname="covForBlue-1D.dat";
+  TString xs_fname="dir-forBlue/xsecForBlue-1D-fromCov.dat";
+  TString cov_fname="dir-forBlue/covForBlue-1D.dat";
+
+  TString tag;
+  switch(normalized) {
+  case 0: tag="1D-absolute"; break;
+  case 1: tag="1D-normalized"; break;
+  case 2: tag="1D-rshape"; break;
+  default:
+    std::cout << "wrong value for normalized=" << normalized << "\n";
+    return;
+  }
+  xs_fname.ReplaceAll("1D",tag);
+  cov_fname.ReplaceAll("1D",tag);
 
   resultCombiner(xs_fname,cov_fname, "41",
 		 xs_fname,cov_fname, "41");
