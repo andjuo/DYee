fb66b22463ca982889fd57b0c5d94489fc432854
Andrius Juodagalvis <andjuo@hotmail.com>
2014-05-30 15:51:42 +0300

    Covariance/chkEventWeight.C: updated macro to check recent implementation of FSR_RND_STUDYid#fixed#

--- Covariance/chkEventWeight.C
+++ Covariance/chkEventWeight.C
@@ -2,6 +2,7 @@
 #include "../Include/MyTools.hh"
 #include "../Include/EventWeight.hh"
 #include "../Include/AccessOrigNtuples.hh"
+#include <TRandom.h>
 
 // -------------------------------------------------------------
 
@@ -67,6 +68,9 @@ void chkEventWeight(int pu_study=1, int the_case=0) {
   else {
     std::cout << "FSR study\n";
     rndStudyStr=Form("FSR_RND_STUDY%d",the_case);
+    if (the_case>100000) {
+      rndStudyStr=Form("FSR_RND_STUDYfixed%d",the_case%100000);
+    }
     std::cout << "test <" << rndStudyStr << ">\n";
 
     if (!ev.init(1,0,DYTools::NO_SYST,rndStudyStr)) {
@@ -76,7 +80,7 @@ void chkEventWeight(int pu_study=1, int the_case=0) {
 
     ev.PrintDetails();
 
-    if (0) {
+    if (1) {
       std::cout << " next=" << ev.nextRndValue() << "\n";
       ev.PrintDetails();
     }
@@ -91,7 +95,42 @@ void chkEventWeight(int pu_study=1, int the_case=0) {
 	ev.PrintDetails();
       }
     }
+    else if (1 && (rndStudyStr.Index("fixed")!=-1)) {
+      HERE("generating a distribution");
+      // Seed-driven FSR_RND (fixed value)
+      TH1D *h= new TH1D("hRnd",Form("file %s",rndStudyStr.Data()),
+			400,0.,2.);
+      if (!h) return;
+      h->SetDirectory(0);
+      int nExps=1000;
+
+      for (int i=0; i<nExps; i++) {
+	//rndStudyStr=Form("FSR_RND_STUDYfixed%d",the_case%100000+17*i);
+	rndStudyStr=Form("FSR_RND_STUDYid%dfixed%7.5lf",i+1,gRandom->Gaus(0,1.));
+
+	if (!ev.init(0,0,DYTools::NO_SYST,rndStudyStr)) {
+	  std::cout << " failure\n";
+	  return;
+	}
+	ev.setSpecWeightValue(accessInfo,1,1.11111);
+	h->Fill(ev.specWeight());
+      }
+
+      TH1D* hchk=(TH1D*)h->Clone("hchk");
+      hchk->Reset();
+      for (int i=0; i<nExps; ++i) hchk->Fill(1+0.05*gRandom->Gaus(0,1.));
+      hchk->SetLineColor(kBlue);
+      hchk->SetMarkerColor(kBlue);
+      hchk->SetMarkerStyle(5);
+
+      TCanvas *cx=new TCanvas("cx1","cx1",800,800);
+      h->SetMarkerStyle(24);
+      h->Draw("LPE");
+      hchk->Draw("LPE same");
+      cx->Update();
+    }
     else {
+      // other FSR RND. Seed driven sequence
       TH1D *h= new TH1D("hRnd",Form("file %s",rndStudyStr.Data()),
 			400,0.,2.);
       h->SetDirectory(0);
