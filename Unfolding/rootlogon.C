9c97f02c2a772ed7cadc038ab266450d7e62d43e
Andrius Juodagalvis <andjuo@hotmail.com>
2014-07-04 18:40:54 +0300

    Unfolding/rootlogon.C added linking of libRooUnfold.so

--- Unfolding/rootlogon.C
+++ Unfolding/rootlogon.C
@@ -2,5 +2,6 @@
 
   gROOT->ProcessLine(".x ../Include/rootlogon.C");
   gROOT->ProcessLine(".L ../Include/FlatIndex.h+");
+  gROOT->ProcessLine(".L libRooUnfold.so");
   gROOT->ProcessLine(".L ../Include/UnfoldingMatrix.h+");
 }
