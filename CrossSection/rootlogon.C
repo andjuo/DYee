906c6effb11210265e19c98f69d092a19f9cbd74
Andrius Juodagalvis <andjuo@hotmail.com>
2014-07-21 16:08:52 +0300

    CrossSection/rootlogon.C added link libRooUnfold.so

--- CrossSection/rootlogon.C
+++ CrossSection/rootlogon.C
@@ -1,6 +1,7 @@
 {  
 
   gROOT->ProcessLine(".x ../Include/rootlogon.C");
+  gROOT->ProcessLine(".L libRooUnfold.so");
   gROOT->ProcessLine(".L crossSectionFnc.cc+");
   gROOT->ProcessLine(".L CSCovWorkFlags.cc+");
   //gROOT->ProcessLine(".L CSCovWorkFlags.hh+");
