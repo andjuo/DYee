dfabc5ffa2683e8d28422b6af788ccfc38e8b6f9
Andrius Juodagalvis <andjuo@hotmail.com>
2014-07-23 14:50:38 +0300

    Unfolding/auxScript.C calls plotUnfoldingMatrixDressed.C

--- Unfolding/auxScript.C
+++ Unfolding/auxScript.C
@@ -1,7 +1,14 @@
+/*
 {
   gROOT->ProcessLine(".L plotUnfoldingMatrix.C+");
   //plotUnfoldingMatrix(1,"../config_files/data_vilnius8TeV.conf.py",DYTools::DEBUG_RUN,DYTools::NO_SYST);
   //plotUnfoldingMatrix(1,"default",DYTools::NORMAL_RUN,DYTools::NO_SYST);
   plotUnfoldingMatrix(1,"default",DYTools::DEBUG_RUN,DYTools::FSR_STUDY);
 
+}
+*/
+
+{
+  gROOT->ProcessLine(".L plotUnfoldingMatrixDressed.C+");
+  plotUnfoldingMatrixDressed(0,"../config_files/data_vilnius8TeV_dressed.conf.py",DYTools::NORMAL_RUN,DYTools::NO_SYST,20.);
 }
